beab78c182815f2e549fa732baa361d664f804b1
Sirwenhao <1547102356@qq.com>
2022-04-03 21:12:57 +0800

    4/3

--- easy/leetcode_0053.py
+++ easy/leetcode_0053.py
@@ -30,27 +30,83 @@
 # 2022/3/26  复习
 # 分治法
 
-import sys
-class Solution:
-    def maxSubArray(self, nums):
-        return self.helper(nums, 0, len(nums) - 1)
-    def helper(self, nums, l, r):
-        if l > r:
-            return -sys.maxsize
-        mid = (l + r) // 2
-        left = self.helper(nums, l, mid - 1)
-        right = self.helper(nums, mid + 1, r)
-        left_suffix_max_sum = right_prefix_max_sum = 0
-        sum = 0
-        for i in reversed(range(l, mid)):
-            sum += nums[i]
-            left_suffix_max_sum = max(left_suffix_max_sum, sum)
-        sum = 0
-        for i in range(mid + 1, r + 1):
-            sum += nums[i]
-            right_prefix_max_sum = max(right_prefix_max_sum, sum)
-        cross_max_sum =left_suffix_max_sum + right_prefix_max_sum + nums[mid]
-        return max(cross_max_sum, left, right)
+# import sys
+# class Solution:
+#     def maxSubArray(self, nums):
+#         return self.helper(nums, 0, len(nums) - 1)
+#     def helper(self, nums, l, r):
+#         if l > r:
+#             return -sys.maxsize
+#         mid = (l + r) // 2
+#         left = self.helper(nums, l, mid - 1)
+#         right = self.helper(nums, mid + 1, r)
+#         left_suffix_max_sum = right_prefix_max_sum = 0
+#         sum = 0
+#         for i in reversed(range(l, mid)):
+#             sum += nums[i]
+#             left_suffix_max_sum = max(left_suffix_max_sum, sum)
+#         sum = 0
+#         for i in range(mid + 1, r + 1):
+#             sum += nums[i]
+#             right_prefix_max_sum = max(right_prefix_max_sum, sum)
+#         cross_max_sum =left_suffix_max_sum + right_prefix_max_sum + nums[mid]
+#         return max(cross_max_sum, left, right)
+
+# 2022/4/3 复习
+# 解法二：前缀和加暴力解，时间复杂度为O(n^2)，空间复杂度为O(N)
+
+# import sys
+# def maxSubArray(nums):
+#     n = len(nums)
+#     maxSum = -sys.maxsize
+#     sum = 0
+#     for i in range(n):
+#         sum = 0
+#         for j in range(i, n):
+#             sum += nums[j]
+#             maxSum = max(maxSum, sum)
+
+#     return maxSum
+
+# # 2022/3/26 复习 
+# '''
+#     1、完全没有思路，
+# '''
+
+# import sys
+# class Solution:
+#     def maxSubArray(self, nums):
+#         return self.helper(nums, 0, len(nums) - 1)
+#     def helper(self, nums, l, r):
+#         if l > r:
+#             return -sys.maxsize
+#         mid = (l + r) // 2
+#         left = self.helper(nums, l , mid - 1)
+#         right = self.helper(nums, mid + 1, r)
+#         left_suffix_max_sum = right_prefix_max_sum = 0
+#         sum = 0
+#         for i in reserved(range(l, mid)):
+#             sum += nums[i]
+#             left_suffix_max_sum = max(left_suffix_max_sum, sum)
+#         sum = 0
+#         for i in range(mid+1, r+1):
+#             sum += nums[i]
+#             right_prefix_max_sum = max(right_prefix_max_sum, sum)
+#         cross_max_sum = left_suffix_max_sum + right_prefix_max_sum + nums[mid]
+#         return max(cross_max_sum, left, right)
+
+
+# 优化前缀和
+
+def maxSubArray(nums):
+    n = len(nums)
+    maxSum = nums[0]
+    minSum = sum = 0
+    for i in range(n):
+        sum += nums[i]
+        maxSum = max(maxSum, sum - minSum) #sum-minSum 
+        minSum = min(minSum, sum)
+
 
 
 nums = [-2,1,-3,4,-1,2,1,-5,4]
--- easy/leetcode_0101.py
+++ easy/leetcode_0101.py
@@ -2,17 +2,33 @@
     1、简单题，想法从中间分开往两边查找
 """
 
+# def isSymmetric(root):
+#     l = len(root)
+#     middle_point = int(l // 2) + 1
+#     for i, j in zip(range(0, middle_point), range(l - 1, middle_point, -1)):
+#         if root[i] == root[j]:
+#             return True
+#         else:
+#             continue
+
+#     return
+
+
+# 2022/4/3 复习
 
 def isSymmetric(root):
-    l = len(root)
-    middle_point = int(l // 2) + 1
-    for i, j in zip(range(0, middle_point), range(l - 1, middle_point, -1)):
-        if root[i] == root[j]:
+    if not root:
+        return True
+    def dfs(left, right):
+        if not (left or right):
             return True
-        else:
-            continue
+        if not (left and right):
+            return False
+        if left.val != right.val:
+            return False
+        return dfs(left.left, right.right) and dfs(left.right, right.left)
 
-    return
+    return dfs(root.left, root.right)
 
 root = [1,2,3,2,2]
 
