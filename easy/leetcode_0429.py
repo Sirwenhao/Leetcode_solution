d44ae28e8fe290204cc285935e5de0041ec8de1d
Sirwenhao <1547102356@qq.com>
2022-04-08 22:09:28 +0800

    4/8

---
+++ easy/leetcode_0429.py
@@ -0,0 +1,24 @@
+"""
+    1、二叉树问题，头疼
+"""
+
+# leetcode官解，对树这种数据结构还是不熟悉
+
+from collections import deque
+
+def levelOrder(root):
+    if not root:
+        return []
+    ans = list()
+    q = deque([root])
+    
+    while q:
+        cnt = len(q)
+        level = list()
+        for _ in range(cnt):
+            cur = q.popleft()
+            level.append(cur.val)
+            for child in cur.children:
+                q.append(level)
+        ans.append(level)
+    return ans
---
+++ easy/leetcode_0455.py
@@ -0,0 +1,25 @@
+"""
+    1、分饼干，首先第一步排序，这个没有考虑到其实，这个要重点记一下
+"""
+
+# 解法一，有参考力扣加加
+def findContentChildren(g: list, s: list):
+    g.sort()
+    s.sort()
+    cnt = 0
+    student = biscuits = 0
+    while student < len(g) and biscuits < len(s):
+        if s[biscuits] >= g[student]:
+            student+=1
+            cnt += 1
+        biscuits += 1
+
+    return cnt
+
+
+
+g = [1,4,3,5,3]
+s = [1,2,2]
+result = findContentChildren(g, s)
+print(result)
+        
