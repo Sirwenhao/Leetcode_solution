fd0a8bfd2fbc8d79606efc3521154f1b7bc7e1b3
Sirwenhao <1547102356@qq.com>
2022-04-23 22:28:02 +0800

    4/23

--- easy/leetcode_0101.py
+++ easy/leetcode_0101.py
@@ -2,30 +2,29 @@
     1、简单题，想法从中间分开往两边查找
 """
 
-# def isSymmetric(root):
-#     l = len(root)
-#     middle_point = int(l // 2) + 1
-#     for i, j in zip(range(0, middle_point), range(l - 1, middle_point, -1)):
-#         if root[i] == root[j]:
-#             return True
-#         else:
-#             continue
-
-#     return
-
-
-# 2022/4/3 复习
-
+# 力扣加加
 def isSymmetric(root):
-    if not root:
-        return True
-    def dfs(left, right):
-        if not (left or right):
-            return True
-        if not (left and right):
-            return False
-        if left.val != right.val:
-            return False
-        return dfs(left.left, right.right) and dfs(left.right, right.left)
-
+    def dfs(root1, root2):
+        if root1 == root2 == None: return True
+        if not root1 and not root2: return False
+        if root1.val != root2.val: return False
+        return dfs(root1.left, root2.right) and dfs(root1.right, root2.left)
+    if not root: return True
     return dfs(root.left, root.right)
+
+
+# # 2022/4/3 复习
+
+# def isSymmetric(root):
+#     if not root:
+#         return True
+#     def dfs(left, right):
+#         if not (left or right):
+#             return True
+#         if not (left and right):
+#             return False
+#         if left.val != right.val:
+#             return False
+#         return dfs(left.left, right.right) and dfs(left.right, right.left)
+
+#     return dfs(root.left, root.right)
