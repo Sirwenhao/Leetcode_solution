20f5102f1e2122a70056bdbe0c21d3e2144e7324
Sirwenhao <1547102356@qq.com>
2022-04-25 00:25:30 +0800

    4/24

--- easy/leetcode_0191.py
+++ easy/leetcode_0191.py
@@ -4,13 +4,19 @@
     3、因此不断循环，直至n二进制全为0即可，统计下循环的次数即为1的个数
     4、值得注意的是：二进制中1的个数被称为这一串二进制数的汉明重量
 '''
+# # 力扣加加
+# def hammingWeight(n):
+#     res = 0
+#     while n:
+#         res += 1
+#         n &= (n-1) #这一步牛逼
+#     return res
+
+# 官解：解法一循环检查二进制位
 
 def hammingWeight(n):
-    res = 0
-    while n:
-        res += 1
-        n &= (n-1) #这一步牛逼
-    return res
+    ans = sum(1 for i in range(32) if n&(1<<i))  # 1<<i表示1左移i位，相当于1乘以2的i次方
+    return ans
 
 n = 0b00000000000000000000000000001011
 
