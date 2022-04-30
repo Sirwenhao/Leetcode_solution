8eb4a522dffa0a6af8643458f40e4277594e9bf6
Sirwenhao <1547102356@qq.com>
2022-04-30 17:51:01 +0800

    4/30

---
+++ middle/leetcode_0002.py
@@ -0,0 +1,106 @@
+"""
+    1、两数相加：涉及知识点为链表
+    2、难点：链表这种数据结构自己还是不知道如何写
+"""
+# # 力扣加加
+# class ListNode:
+#     def __init__(self, val=0, next=None) -> None:
+#         self.val = val
+#         self.next = next
+
+# class Solution():
+#     def addTwoNubmers(self, l1: ListNode, l2: ListNode):
+#         """
+#         type l1: ListNode
+#         type l2: ListNode
+#         rtype: ListNode
+#         """
+#         res = ListNode(0)
+#         head = res
+#         carry = 0
+#         while l1 or l2 or carry != 0:
+#             sum = carry
+#             if l1:
+#                 sum += l1.val
+#                 l1 = l1.next
+#             if l2:
+#                 sum += l2.val
+#                 l2 = l2.next
+
+#             if sum <= 9:
+#                 res.val = sum
+#                 carry = 0
+#             else:
+#                 res.val = sum % 10
+#                 carry = sum // 10
+
+#             # creat new node
+#             if l1 or l2 or carry != 0:
+#                 res.next = ListNode(0)
+#                 res = res.next
+#         return head
+
+# if __name__ == '__main__':
+#     l1 = ListNode[2,3,4]
+#     l2 = ListNode[4,5,6]
+#     result = Solution().addTwoNubmers(l1, l2)
+#     print(result)
+
+
+# # 2022/4/30 review
+# class Solution:
+#     def addTwoNumbers(self, l1, l2):
+#         dummy = ListNode(0)
+#         carry, cur = 0, dummy
+#         while l1 or l2 or carry != 0:
+#             if not l1:
+#                 sum = carry
+#             else:
+#                 sum = l1.val + carry
+
+#             if not l2:
+#                 sum = carry
+#             else:
+#                 sum = l2.val + carry
+
+#             carry, val = divmod(sum, 10)
+#             cur.next = ListNode(val)
+#             cur = cur.next
+#             l1 = None if not l1 else l1.next
+#             l2 = None if not l2 else l2.next
+#         return dummy.next
+
+
+# 爱学习的饲养员解法
+# 解法一：迭代法，容易超时
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        total = 0
+        carry = 0
+        dummy = ListNode()
+        cur = dummy
+        while(l1 != None and l2 != None):
+            total = l1.val + l2.val + carry
+            cur.next = ListNode(total % 10)
+            carry = total // 10
+            cur = cur.next
+            l1 = l1.next
+            l2 = l2.next
+
+        while l1 != None:
+            total = l1.val + carry
+            cur.next = ListNode(total % 10)
+            carry = total // 10
+            cur = cur.next
+            l1 = l1.next
+
+        while l2 != None:
+            total = l2.val + carry
+            cur.next = ListNode(total % 10)
+            carry = total // 10
+            cur = cur.next
+            l2 = l2.next
+
+        if carry != 0:
+            cur.next = ListNode(carry)
+        return dummy.next
--- middle/leetcode_0046.py
+++ middle/leetcode_0046.py
@@ -12,41 +12,41 @@
 #         import itertools
 #         return itertools.permutations(nums)
 
-# # 力扣加加解法二：回溯法
-# class Solution:
-#     def permute(self, nums):
-#         """自己写回溯法"""
-#         res = []
-#         def backtrack(nums, pre_list):
-#             if len(nums) <= 0:
-#                 res.append(pre_list)
-#             else:
-#                 for i in nums:
-#                     # 注意copy一份新的调用，否则无法正常循环
-#                     p_list = pre_list.copy()
-#                     p_list.append(i)
-#                     left_nums = nums.copy()
-#                     left_nums.remove(i)
-#                     backtrack(left_nums, p_list)
-#         backtrack(nums, [])
-#         return res
-
-
-# 力扣加加解法三：回溯
+# 力扣加加解法二：回溯法
 class Solution:
     def permute(self, nums):
+        """自己写回溯法"""
         res = []
-        length = len(nums)
-        def backtrack(start=0):
-            if start == length:
-                # nums[:]返回nuns的一个副本，指向新的引用，
-                res.append(nums[:])
-            for i in range(start, length):
-                nums[start], nums[i] = nums[i], nums[start]
-                backtrack(start+1)
-                nums[start], nums[i] = nums[i], nums[start]
-        backtrack()
+        def backtrack(nums, pre_list):
+            if len(nums) <= 0:
+                res.append(pre_list)
+            else:
+                for i in nums:
+                    # 注意copy一份新的调用，否则无法正常循环
+                    p_list = pre_list.copy()
+                    p_list.append(i)
+                    left_nums = nums.copy()
+                    left_nums.remove(i)
+                    backtrack(left_nums, p_list)
+        backtrack(nums, [])
         return res
+
+
+# # 力扣加加解法三：回溯
+# class Solution:
+#     def permute(self, nums):
+#         res = []
+#         length = len(nums)
+#         def backtrack(start=0):
+#             if start == length:
+#                 # nums[:]返回nuns的一个副本，指向新的引用，
+#                 res.append(nums[:])
+#             for i in range(start, length):
+#                 nums[start], nums[i] = nums[i], nums[start]
+#                 backtrack(start+1)
+#                 nums[start], nums[i] = nums[i], nums[start]
+#         backtrack()
+#         return res
 
 
 if __name__ == "__main__":
