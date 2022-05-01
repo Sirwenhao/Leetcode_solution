c8853e0b9ce36cea2fb08db5c729d463350a5fae
Sirwenhao <1547102356@qq.com>
2022-05-01 17:08:59 +0800

    5/1

--- Python_Solution/middle/leetcode_0011.py
+++ Python_Solution/middle/leetcode_0011.py
@@ -14,22 +14,49 @@
 
 # 解法二：官解提示和力扣加加都提示了双指针，自己写
 # 时间复杂度：O(N)
-def maxAre(height):
-    left = 0
-    right = len(height)-1
-    max_v = 0
-    while left < right:
-        max_v = max(max_v, min(height[right], height[left]) * (right-left))
-        if height[left] < height[right]:
-            left += 1
-        else:
-            right -= 1
-    return max_v
+# class Solution:
+# def maxAre(self, height):
+#     left = 0
+#     right = len(height)-1
+#     max_v = 0
+#     while left < right:
+#         max_v = max(max_v, min(height[right], height[left]) * (right-left))
+#         if height[left] < height[right]:
+#             left += 1
+#         else:
+#             right -= 1
+#     return max_v
 
+# # 2022/5/1 review
+# # 双指针
+# class Solution:
+#     def maxArea(self, heights):
+#         left, right = 0, len(heights)-1
+#         max_v = 0
+#         while left < right:
+#             max_v = max(max_v, (right-left) * min(heights[right], heights[left]))
+#             if heights[left] > heights[right]: # 实际参与计算的高度是由最低的高度所主导的
+#                 left += 1
+#             if heights[left] <= heights[right]:
+#                 right -= 1
+#         return max_v
 
+# 双指针
+class Solution:
+    def maxArea(self, height):
+        i, j = 0, len(height)-1
+        res = 0
+        while i < j:
+            t = (j-i)*min(height[i], height[j])
+            res = max(res, t)
+            if height[i] < height[j]:
+                i += 1
+            else:
+                j -= 1
+        return res
 
-
-
-height = [1,8,6,2,5,4,8,3,7]
-result = maxAre(height)
-print(result)
+if __name__ == "__main__":
+    # heights = [1,8,6,2,5,4,8,3,7]
+    heights = [1,2,1]
+    result = Solution().maxArea(heights)
+    print(result)
