76bebe3a73e654068e75802d6c5543a851d4526b
Sirwenhao <1547102356@qq.com>
2022-05-07 20:06:16 +0800

    5/7

--- Python_Solution/middle/leetcode_0040.py
+++ Python_Solution/middle/leetcode_0040.py
@@ -40,36 +40,36 @@
 
 # 2022/5/7 0:12 Author:WH 回溯
 class Solution:
-    def __init__(self):
-        self.ans = []  # 用于存放结果集
-        self.current = []  # 用于考虑当前情况
-
     def combinationSum2(self, candidates, target):
-        self.ans.clear()
-        self.current.clear()
+        ans = []
+        current = []
         # 排序这个关键步骤不能少
         candidates.sort()
-        self.backtracking(candidates, target, 1)
-        return self.ans
 
-    # 定义回溯部分主体
-    def backtracking(self, candidates, target, start_index):
-        # base condition
-        if start_index == len(candidates):
-            self.ans.append(self.current[:])  # 此处用浅拷贝
-            return # 这个return语句被我遗忘了
+        # 定义回溯部分主体
+        def backtracking(candidates, target, start_index, current):
+            # base condition
+            if sum(current) == target:
+                ans.append(current[:])  # 此处用浅拷贝
+                return # 这个return语句被我遗忘了
 
-        for i in range(len(candidates)):
-            self.current.append(candidates[i])
-            if sum(self.current) == target:
-                self.ans += self.current
-            self.backtracking(candidates, target, start_index+1)
-            self.current.pop()
-            
+            for i in range(start_index, len(candidates)):  # 此处的start_index是防止重复取元素
+                # 剪枝操作
+                if sum(current) > target:
+                    return
+                # 剪枝操作：跳过同一树层使用过的元素
+                if i > start_index and candidates[i] == candidates[i-1]:
+                    continue
+                current.append(candidates[i])
+                backtracking(candidates, target, i+1, current)
+                current.pop()
+
+        backtracking(candidates, target, 0, current)
+        return ans
 
 
 if __name__ == '__main__':
-    candidates = [2,5,2,1,2]
+    candidates = [5,2,2,1,2]
     target = 5
     result = Solution().combinationSum2(candidates, target)
     print(result)
