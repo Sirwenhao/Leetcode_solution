5afe555f175340e80b00f5bb406886e6171c2529
Sirwenhao <1547102356@qq.com>
2022-05-19 21:14:30 +0800

    5/19

--- Python_Solution/middle/leetcode_0131.py
+++ Python_Solution/middle/leetcode_0131.py
@@ -4,35 +4,27 @@
 """
 # 回溯法 author:WH
 class Solution:
+    def __init__(self) -> None:
+        self.ans = []
+        self.current = []
     def partition(self, s):
-        ans = []
-        current = []
-        start_index = 0
-        # 定义一个判断是否满足回文的函数
-        def isPalindrome(str):
-            left, right = 0, len(str)-1
-            while left < right:
-                if str[left] != str[right]:
-                    return False
-                left += 1
-                right -= 1
-            return True
-        # 定义回溯函数主体
+        self.ans.clear()
+        self.current.clear()
+        # 定义回溯函数结构
         def backtracking(s, start_index):
             if start_index >= len(s):
-                ans.append(current[:])
+                self.ans.append(self.current[:])
                 return
-            # 定义单层循环
             for i in range(start_index, len(s)):
-                if isPalindrome(s):
-                    current += s[start_index:i+1]
-                    start_index += 1
-                    backtracking(s, start_index)
-                    current.pop()
+                if s[start_index:i+1][::1] == s[start_index:i+1][::-1]:
+                    self.current.append(s[start_index:i+1])
+                    backtracking(s, i+1)
+                    self.current.pop()
                 else:
                     continue
-        backtracking(s, start_index)
-        return ans
+        backtracking(s, 0)
+        return self.ans
+        
 
 if __name__ == "__main__":
     s = "aab"
