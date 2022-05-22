927fc3a9aaaaf0bb2161f1d90783b1f0c12acb74
Sirwenhao <1547102356@qq.com>
2022-05-22 17:42:02 +0800

    5/22

--- Python_Solution/middle/leetcode_0077.py
+++ Python_Solution/middle/leetcode_0077.py
@@ -3,23 +3,66 @@
     2、k代表数字长度，n代表数字范围上限
 """   
 
-class Solution:
-    def combine(self, n, k):
-        nums = [i+1 for i in range(n)]
-        ans = []
-        current = []
-        start_index = 0
-        def backtracking(nums, start_index):
-            if len(current) == k:
-                ans.append(current[:])
-            for i in range(start_index, len(nums)):
-                current.append(nums[i])
-                start_index += 1
-                backtracking(nums, start_index)
-                current.pop()
-        backtracking(nums, start_index)
-        return ans
+# class Solution:
+#     def combine(self, n, k):
+#         nums = [i+1 for i in range(n)]
+#         ans = []
+#         current = []
+#         start_index = 0
+#         def backtracking(nums, start_index):
+#             if len(current) == k:
+#                 ans.append(current[:])
+#             for i in range(start_index, len(nums)):
+#                 current.append(nums[i])
+#                 start_index += 1
+#                 backtracking(nums, start_index)
+#                 current.pop()
+#         backtracking(nums, start_index)
+#         return ans
 
+# # 2022/5/21 author:WH
+# class Solution:
+#     def __init__(self):
+#         self.ans = []
+#         self.current = []
+
+#     def combine(self, n, k):
+#         self.ans.clear()
+#         self.current.clear()
+#         self.backtracking(n, k, 1)
+#         return self.ans
+
+#     def backtracking(self, n, k, start_index):
+#         if len(self.current) == k:
+#             self.ans.append(self.current[:])
+#             return
+
+#         for i in range(start_index, n+1):
+#             self.current.append(i)
+#             self.backtracking(n, k, i+1)
+#             self.current.pop()
+
+# 剪枝优化的版本
+class Solution:
+    def __init__(self):
+        self.ans = []
+        self.current = []
+
+    def combine(self, n, k):
+        self.ans.clear()
+        self.current.clear()
+        self.backtracking(n, k, 1)
+        return self.ans
+
+    def backtracking(self, n, k, start_index):
+        if len(self.current) == k:
+            self.ans.append(self.current[:])
+            return
+
+        for i in range(start_index, n-(k-len(self.current))+2):
+            self.current.append(i)
+            self.backtracking(n, k, i+1)
+            self.current.pop()     
 
 if __name__ == "__main__":
     n = 4
