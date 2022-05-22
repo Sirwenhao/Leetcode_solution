ccc545fefe00f9560a00fed2dbab6509e7fdfc06
Sirwenhao <1547102356@qq.com>
2022-05-22 17:41:56 +0800

    5/22

--- Python_Solution/middle/leetcode_0040.py
+++ Python_Solution/middle/leetcode_0040.py
@@ -38,38 +38,65 @@
 #                 path.pop()
 
 
-# 2022/5/7 0:12 Author:WH 回溯
+# # 2022/5/7 0:12 Author:WH 回溯
+# class Solution:
+#     def combinationSum2(self, candidates, target):
+#         ans = []
+#         current = []
+#         # 排序这个关键步骤不能少
+#         candidates.sort()
+
+#         # 定义回溯部分主体
+#         def backtracking(candidates, target, start_index, current):
+#             # base condition
+#             if sum(current) == target:
+#                 ans.append(current[:])  # 此处用浅拷贝
+#                 return # 这个return语句被我遗忘了
+
+#             for i in range(start_index, len(candidates)):  # 此处的start_index是防止重复取元素
+#                 # 剪枝操作
+#                 if sum(current) > target:
+#                     return
+#                 # 剪枝操作：跳过同一树层使用过的元素
+#                 if i > start_index and candidates[i] == candidates[i-1]:
+#                     continue
+#                 current.append(candidates[i])
+#                 backtracking(candidates, target, i+1, current)
+#                 current.pop()
+
+#         backtracking(candidates, target, 0, current)
+#         return ans
+
+# 2022/5/21 author:WH
 class Solution:
+    def __init__(self):
+        self.ans = []
+        self.current = []
+
     def combinationSum2(self, candidates, target):
-        ans = []
-        current = []
-        # 排序这个关键步骤不能少
         candidates.sort()
+        self.ans.clear()
+        self.current.clear()
+        self.backtracking(candidates, target, 0)
+        return self.ans
 
-        # 定义回溯部分主体
-        def backtracking(candidates, target, start_index, current):
-            # base condition
-            if sum(current) == target:
-                ans.append(current[:])  # 此处用浅拷贝
-                return # 这个return语句被我遗忘了
-
-            for i in range(start_index, len(candidates)):  # 此处的start_index是防止重复取元素
-                # 剪枝操作
-                if sum(current) > target:
-                    return
-                # 剪枝操作：跳过同一树层使用过的元素
-                if i > start_index and candidates[i] == candidates[i-1]:
-                    continue
-                current.append(candidates[i])
-                backtracking(candidates, target, i+1, current)
-                current.pop()
-
-        backtracking(candidates, target, 0, current)
-        return ans
-
+    def backtracking(self, candidates, target, start_index):
+        if sum(self.current) == target:
+            self.ans.append(self.current[:])
+            return
+        for i in range(start_index, len(candidates)):
+            # 剪枝
+            if sum(self.current) > target:
+                return
+            # 去重，这个地方的i>start_index没有想明白
+            if i > start_index and candidates[i] == candidates[i-1]:
+                continue
+            self.current.append(candidates[i])
+            self.backtracking(candidates, target, i+1)
+            self.current.pop()
 
 if __name__ == '__main__':
-    candidates = [5,2,2,1,2]
-    target = 5
+    candidates = [10,1,2,7,6,1,5]
+    target = 8
     result = Solution().combinationSum2(candidates, target)
     print(result)
