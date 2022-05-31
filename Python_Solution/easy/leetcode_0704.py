279077269a8794e48e6a743064c10bdb937b9f6f
Sirwenhao <1547102356@qq.com>
2022-05-31 23:13:22 +0800

    5/31

--- Python_Solution/easy/leetcode_0704.py
+++ Python_Solution/easy/leetcode_0704.py
@@ -1,22 +1,40 @@
 """
     1、二分查找
     2、更新的的是左右边界，中心middle值是左右两个边界的均值
+    3、属于leetcode专题中的数组类
 """
 
 # 2022/5/9 author:WH
+# class Solution:
+#     def search(self, nums, target):
+#         nums.sort()
+#         left, right = 0, len(nums)-1
+#         while left < right:
+#             middle = (left+right) // 2
+#             if nums[middle] < target:
+#                 left = middle+1
+#             elif nums[middle] > target:
+#                 right = middle
+#             else:
+#                 return middle
+#         return -1
+
+# 2022/5/31 author:WH
 class Solution:
     def search(self, nums, target):
-        nums.sort()
         left, right = 0, len(nums)-1
+        # 千万不要忘记先进行排序
+        nums.sort()
         while left < right:
-            middle = (left+right) // 2
-            if nums[middle] < target:
-                left = middle+1
-            elif nums[middle] > target:
+            middle = (left + right) // 2
+            if nums[middle] > target:
                 right = middle
+            elif nums[middle] < target:
+                left = middle
             else:
                 return middle
         return -1
+        
 
 if __name__ == '__main__':
     nums = [1,2,3,4,5,6]
