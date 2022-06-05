24af077737d4d2fec96925b3fbd94eb31906bb9d
Sirwenhao <1547102356@qq.com>
2022-06-05 12:05:22 +0800

    6/5实验室出游结束

---
+++ Python_Solution/easy/leetcode_0977.py
@@ -0,0 +1,24 @@
+"""
+    1、有序数组的平方
+"""
+# 2022/6/5  author:WH
+class Solution:
+    def sortedSquares(self, nums):
+        left, right, k = 0, len(nums)-1, len(nums)-1
+        new_Nums = [0]*len(nums)
+        while left <= right:
+            a = nums[left] ** 2
+            b = nums[right] ** 2
+            if a <= b:
+                new_Nums[k] = b
+                right -= 1
+            else:
+                new_Nums[k] = a
+                left += 1
+            k -= 1
+        return new_Nums
+
+if __name__ == "__main__":
+    nums = [-4, -1, 0, 3, 10]
+    result = Solution().sortedSquares(nums)
+    print(result)
