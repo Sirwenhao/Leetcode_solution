f35f2b890bebf7d79ccb80036aac11278a783e53
Sirwenhao <1547102356@qq.com>
2022-06-08 20:26:04 +0800

    6/8

--- Python_Solution/middle/leetcode_0707.py
+++ Python_Solution/middle/leetcode_0707.py
@@ -24,10 +24,10 @@ class MyLinkedList:
             return -1
 
     def addAtHead(self, val):
-        self.addAtHead(0, val)
+        self.addAtIndex(0, val)
 
     def addAtTail(self, val):
-        self.addAtTail(self._count, val)
+        self.addAtIndex(self._count, val)
 
     def addAtIndex(self, index, val):
         if index < 0:
@@ -52,3 +52,65 @@ class MyLinkedList:
                 prev_node, current_node = current_node, current_node.next
             else:
                 prev_node.next, current_node.next = current_node.next, None
+
+
+# 2022/6/8  author:WH
+# 双链表
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class MyLinkedList:
+    def __init__(self):
+        self._head, self._tail = Node(0), Node(0)
+        self._head.next, self._tail.prev = self._tail, self._head
+        self._count = 0
+
+    def _get_node(self, index):
+        # 当index小于_count//2时，使用_head查找更快，反之_tail更快
+        if index >= self._count // 2:
+            # 使用prev往前查找
+            node = self._tail
+            for _ in range(self._count - index):
+                node = node.prev
+        else:
+            # 使用next往后找
+            node = self._head
+            for _ in range(index + 1):
+                node = node.next
+        return node
+
+    def get(self, index):
+        if 0 <= index < self._count:
+            node = self._get_node(index)
+            return node.val
+        else:
+            return -1
+
+    def addAtHead(self, val):
+        self._update(self._head, self._head.next, val)
+
+    def addAtTail(self, val):
+        self._update(self._tail.prev, self._tail, val)
+
+    def addAtIndex(self, index, val):
+        if index < 0:
+            index = 0
+        elif index > self._count:
+            return
+        node = self._get_node(index)
+        self._update(node.prev, node, val)
+
+    def _update(self, prev, next, val):
+        self._count += 1
+        node = Node(val)
+        prev.next, next.prev = node, node
+        node.prev, node.next = prev, next
+
+    def deleteAtIndex(self, index):
+        if 0 <= index < self._count:
+            node = self._get_node(index)
+            self._count -= 1
+            node.prev.next, node.next.prev = node.next, node.prev
