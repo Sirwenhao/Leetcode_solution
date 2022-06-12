e5e0be1e2d1a4e38cc665a84df29765e98e0481e
Sirwenhao <1547102356@qq.com>
2022-06-12 20:52:21 +0800

    6/12

---
+++ Python_Solution/easy/leetcode_0242.py
@@ -0,0 +1,27 @@
+"""
+1、哈希表专题
+2、字母异位序：两个字符串其中所包含的字符出现次数相同
+"""
+# 初步想法：遍历字符串s，把s中的字符作为键，其出现次数作为值，存成字典
+class Solution:
+    def isAnagram(self, s, t):
+        record = [0] * 26
+        # 这个操作可以统计所有字符出现的频次
+        for i in range(len(s)):
+            record[ord(s[i]) - ord("a")] += 1
+        print(record)
+        for i in range(len(t)):
+            record[ord(t[i]) - ord("a")] -= 1
+
+        for i in range(26):
+            if record[i] != 0:
+                return False
+                break      
+        return True
+
+if __name__ == "__main__":
+    s = 'recordsdss'
+    t = 'recordsdss'
+    result = Solution().isAnagram(s, t)
+    print(result)
+        
