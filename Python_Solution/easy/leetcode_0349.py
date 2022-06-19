a798ac6557363026da171e3d5703fafa1c8fa792
Sirwenhao <1547102356@qq.com>
2022-06-19 19:35:27 +0800

    6/19

--- Python_Solution/easy/leetcode_0349.py
+++ Python_Solution/easy/leetcode_0349.py
@@ -28,12 +28,24 @@
 # print(result)
 
 # 2022/6/12  author:WH
+# class Solution:
+#     def intersection(self, nums1, nums2):
+#         # 使用set哈希集合
+#         nums1 = set(nums1)
+#         nums2 = set(nums2)
+#         return list(nums1 & nums2)
+
+# 2022/6/19  author:WH
 class Solution:
     def intersection(self, nums1, nums2):
-        # 使用set哈希集合
-        nums1 = set(nums1)
-        nums2 = set(nums2)
-        return list(nums1 & nums2)
+        res = []
+        nums1 = list(set(nums1))
+        nums2 = list(set(nums2))
+        for i in range(len(nums1)):
+            if nums1[i] in nums2:
+                res.append([nums1[i]])
+        return res
+
 if __name__ == "__main__":
     nums1 = [1,2,3,4,5,6,2,1]
     nums2 = [1,2,3,5]
