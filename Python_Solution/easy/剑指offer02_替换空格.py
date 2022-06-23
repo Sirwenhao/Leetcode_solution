7a138207989113df6b836af768f655590f0896c9
Sirwenhao <1547102356@qq.com>
2022-06-23 22:19:10 +0800

    6/23

---
+++ Python_Solution/easy/剑指offer02_替换空格.py
@@ -0,0 +1,29 @@
+"""
+    1、剑指0ffer05-替换空格
+"""
+# 2022/6/23  author:WH
+# class Solution:
+#     def replaceSpace(self, s):
+#         for i in range(len(s)):
+#             if s[i] == " ":
+#                 s = s[:i] + "%20" + s[i+1:]
+#         return s
+
+# 2022/6/23  author:代码随想录
+class Solution:
+    def replaceSpace(self, s):
+        return "%20".join(s.split(" "))
+
+        n = len(s)
+        for e, i in enumerate(s[::-1]):
+            print(i, e)
+            if i == " ":
+                s = s[:n - (e+1)] + "%20" + s[n-2:]
+            print("")
+        return s
+
+if __name__ == "__main__":
+    # s = "We are happy."
+    s = "     "
+    result = Solution().replaceSpace(s)
+    print(result)
