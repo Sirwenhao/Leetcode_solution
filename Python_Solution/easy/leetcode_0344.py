a2191da35b3135f9fb25e6ad404201e493dd76b6
Sirwenhao <1547102356@qq.com>
2022-06-23 20:27:01 +0800

    6/23

---
+++ Python_Solution/easy/leetcode_0344.py
@@ -0,0 +1,15 @@
+# 2022/6/23  author:WH
+# 考虑使用双指针
+class Solution:
+    def reverseString(self, s):
+        l, r = 0, len(s)-1
+        while l <= r:
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
+        return s  # 这个地方可以不用返回s，因为本来就是直接在字符串s上进行操作的
+
+if __name__ == "__main__":
+    s = ["h", "e", "l", "l", "o"]
+    result = Solution().reverseString(s)
+    print(result)
