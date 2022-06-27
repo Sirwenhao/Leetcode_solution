02242aeb421e8022c69893b435d92db5358e6d5c
Sirwenhao <1547102356@qq.com>
2022-06-27 21:25:54 +0800

    6/27

--- Python_Solution/easy/剑指offer58-II.py
+++ Python_Solution/easy/剑指offer58-II.py
@@ -2,13 +2,18 @@
 1、剑指offer58II左旋字符串
 """
 # 2022/6/27  author:WH
+# class Solution:
+#     def reverseLeftWords(self, s, n):
+#         step = n % len(s)
+#         ans = s[step:] + s[:step]
+#         print(s[step:])
+#         print(s[:step])
+#         return ans
+
+# 2022/6/27  author:代码随想录
 class Solution:
     def reverseLeftWords(self, s, n):
-        step = n % len(s)
-        ans = s[step:] + s[:step]
-        print(s[step:])
-        print(s[:step])
-        return ans
+        return s[n:] + s[:n]
 
 if __name__ == "__main__":
     # s = "abcdefg"
