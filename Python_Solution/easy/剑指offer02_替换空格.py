9220bd7cfe0c0532c50e699c9854e60b4749b049
Sirwenhao <1547102356@qq.com>
2022-06-28 21:29:59 +0800

    6/28

--- Python_Solution/easy/剑指offer02_替换空格.py
+++ Python_Solution/easy/剑指offer02_替换空格.py
@@ -10,20 +10,30 @@
 #         return s
 
 # 2022/6/23  author:代码随想录
+# class Solution:
+#     def replaceSpace(self, s):
+#         return "%20".join(s.split(" "))
+
+#         n = len(s)
+#         for e, i in enumerate(s[::-1]):
+#             print(i, e)
+#             if i == " ":
+#                 s = s[:n - (e+1)] + "%20" + s[n-2:]
+#             print("")
+#         return s
+
+# if __name__ == "__main__":
+#     # s = "We are happy."
+#     s = "     "
+#     result = Solution().replaceSpace(s)
+#     print(result)
+
+
 class Solution:
     def replaceSpace(self, s):
         return "%20".join(s.split(" "))
-
-        n = len(s)
-        for e, i in enumerate(s[::-1]):
-            print(i, e)
-            if i == " ":
-                s = s[:n - (e+1)] + "%20" + s[n-2:]
-            print("")
-        return s
-
+    
 if __name__ == "__main__":
-    # s = "We are happy."
-    s = "     "
+    s = "we are the world!"
     result = Solution().replaceSpace(s)
     print(result)
