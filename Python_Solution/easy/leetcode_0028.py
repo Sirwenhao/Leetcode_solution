ad742fae7c68d526bd02d638f867f17f5347ae64
Sirwenhao <1547102356@qq.com>
2022-06-28 14:53:25 +0800

    6/28

--- Python_Solution/easy/leetcode_0028.py
+++ Python_Solution/easy/leetcode_0028.py
@@ -4,15 +4,47 @@
 """
 # 2022/6/28  author:WH
 # 不使用KMP的解法
+# class Solution:
+#     def strStr(self, haystack, needle):
+#         l = len(needle)
+#         for i in range(len(haystack)):
+#             if haystack[i] == needle[0]:
+#                 while haystack[i:i+len(needle)] == needle:
+#                     return i
+#         return -1
+
+# 使用KMP算法
+from numpy import kaiser
+
+
 class Solution:
     def strStr(self, haystack, needle):
-        l = len(needle)
-        for i in range(len(haystack)):
-            if haystack[i] == needle[0]:
-                while haystack[i:i+len(needle)] == needle:
-                    return i
+        a = len(haystack)
+        b = len(needle)
+        if a == 0:
+            return 0
+        next = self.getnext(a, needle)
+        p = -1
+        for j in range(b):
+            while p >= 0 and needle[p+1] != haystack[j]:
+                p = next[p]
+            if needle[p+1] == haystack[j]:
+                p+=1
+            if p==a-1:
+                return j-a+1
         return -1
 
+    def getnext(self, a, needle):
+        next = ['' for i in range(a)]
+        k=-1
+        next[0] = k
+        for i in range(1, len(needle)):
+            while (k>-1 and needle[k+1]!=needle[i]):
+                k = next[k]
+            if needle[k+1]==needle[i]:
+                k+=1
+            next[i] = k
+        return next
 if __name__ == "__main__":
     haystack = "aaaaa"
     needle = "bba"
