05e58fd1a84583d1ac3ab40b9ab851c3c19282f8
Sirwenhao <1547102356@qq.com>
2022-07-01 21:19:28 +0800

    7/1

--- Python_Solution/easy/leetcode_0232.py
+++ Python_Solution/easy/leetcode_0232.py
@@ -3,37 +3,71 @@
     2、复习栈和队列两种数据结构
 """
 
-class MyQueue:
+# class MyQueue:
 
-    def __init__(self) -> None:
-        self.stack = []
-        self.help_stack = []
+#     def __init__(self) -> None:
+#         self.stack = []
+#         self.help_stack = []
+
+#     def push(self, x):
+#         """
+#         push element to back of queue
+#         """
+#         while self.stack:
+#             self.help_stack.append(self.stack.pop())
+#         self.help_stack.append(x)
+#         while self.help_stack:
+#             self.stack.append(self.help_stack.pop())
+
+
+#     def pop(self):
+#         """
+#         remove the element from in front of queue and returns thar element
+#         """
+#         return self.stack.pop()
+
+#     def peek(self):
+#         """
+#         get the front element
+#         """
+#         return self.stack[-1]
+
+#     def empty():
+#         """
+#         returns whether the queue is empty
+#         """
+#         return not bool(self.stack)
+
+
+# 2022/7/1  author:WH
+# 栈与队列专题
+# 栈是先进后出，队列是先进先出。要用两个栈实现队列实现先进后出，两个栈数据互传
+class MyQueue:
+    def __init__(self):
+        # in主要负责push，out负责pop
+        self.stack_in = [] # 用于push进入元素
+        self.stack_out = [] # 用于pop出去元素
 
     def push(self, x):
-        """
-        push element to back of queue
-        """
-        while self.stack:
-            self.help_stack.append(self.stack.pop())
-        self.help_stack.append(x)
-        while self.help_stack:
-            self.stack.append(self.help_stack.pop())
-
+        self.stack_in.append(x)
 
     def pop(self):
-        """
-        remove the element from in front of queue and returns thar element
-        """
-        return self.stack.pop()
+        # 如果stack_out为空，就不返回任何参数
+        if self.empty():
+            return None
 
-    def peek(self):
-        """
-        get the front element
-        """
-        return self.stack[-1]
+        # 如果stack_out不为空，就直接pop元素
+        if self.stack_out:
+            return self.stack_out.pop()
+        else: # 如果stack_out为空，就把stack_in中的元素放入到stack_out中用于输出
+            # 通过循环把stack_in中的元素完全放入到stack_in中，刚好又实现了一次先进后出
+            # 两次先进后出的叠加共同实现了先进先出
+            for i in range(len(self.stack_in)):
+                self.stack_out.append(self.stack_in.pop())
+            return self.stack_out.pop()
 
-    def empty():
-        """
-        returns whether the queue is empty
-        """
-        return not bool(self.stack)
+    def empty(self):
+        # 只要stack_in或者stack_out中有元素就表示不为空
+        # if self.stack_in or self.stack_out:
+        #     return False
+        return not (self.stack_in or self.stack_out)
