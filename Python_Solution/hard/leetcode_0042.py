4ec7ea7fec0377f1fc762024b3eb6308a9c9e288
Sirwenhao <1547102356@qq.com>
2022-07-16 17:51:24 +0800

    7/16

--- Python_Solution/hard/leetcode_0042.py
+++ Python_Solution/hard/leetcode_0042.py
@@ -20,21 +20,57 @@
 
 # 2022/7/13  author:WH
 # 参考leetcode官解双指针版本
+# class Solution:
+#     def trap(self, height):
+#         ans = 0
+#         left, right = 0, len(height) - 1
+#         leftMax = rightMax = 0
+#         while left < right:
+#             leftMax = max(leftMax, height[left])
+#             rightMax = max(rightMax, height[right])
+#             if height[left] < height[right]:
+#                 ans += leftMax - height[left]
+#                 left += 1
+#             else:
+#                 ans += rightMax - height[right]
+#                 right -= 1
+#         return ans
+
+# 2022/7/16  author:WH
+# 当前位置的蓄水量，取决于左右两边蓄水量的最小值与当前位置高度之间的差值
+# class Solution:
+#     def trap(self, height):
+#         ans = 0
+#         left, right = 0, len(height)-1
+#         leftMax = rightMax = 0
+#         while left < right:
+#             leftMax = max(leftMax, height[left])
+#             rightMax = max(rightMax, height[right])
+#             if height[left] < height[right]:
+#                 ans += leftMax - height[left]
+#                 left += 1
+#             else:
+#                 ans += rightMax - height[right]
+#                 right -= 1
+#         return ans
+
 class Solution:
     def trap(self, height):
-        ans = 0
-        left, right = 0, len(height) - 1
-        leftMax = rightMax = 0
-        while left < right:
-            leftMax = max(leftMax, height[left])
-            rightMax = max(rightMax, height[right])
-            if height[left] < height[right]:
-                ans += leftMax - height[left]
-                left += 1
-            else:
-                ans += rightMax - height[right]
-                right -= 1
-        return ans
+        n = len(height)
+        if n < 3:
+            return 0
+        left_max = [height[0]] * n
+        for i in range(1, n):
+            left_max[i] = max(left_max[i - 1], height[i])
+            
+        right_max = [height[n - 1]] * n
+        for i in range(n - 2, -1, -1):
+            right_max[i] = max(right_max[i + 1], height[i])
+            
+        res = 0
+        for i in range(n):
+            res += min(left_max[i], right_max[i]) - height[i]
+        return res
 
 if __name__ == "__main__":
     height = [0,1,0,2,1,0,1,3,2,1,2,1]
