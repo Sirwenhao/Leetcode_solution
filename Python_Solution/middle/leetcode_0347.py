2a118454f4d5c5ac983d2ef58998a2be1610bc79
Sirwenhao <1547102356@qq.com>
2022-07-16 17:16:54 +0800

    7/16

--- Python_Solution/middle/leetcode_0347.py
+++ Python_Solution/middle/leetcode_0347.py
@@ -22,28 +22,42 @@
 #         return ans
 
 # 2022/7/13  author:代码随想录
+# import heapq
+# class Solution:
+#     def topKFrequent(self, nums, k):
+#         # 统计元素出现的频率
+#         map_ = {}
+#         for i in range(len(nums)):
+#             map_[nums[i]] = map_.get(nums[i], 0) + 1
+#         # 对频率排序，定义一个小顶堆，大小为k
+#         pri_que = []
+#         # 用固定大小为k的小顶堆，扫描所有频率的数值
+#         for key, freq in map_.items():
+#             heapq.heappush(pri_que, (freq, key))
+#             # 如果堆的大小大于了k，则队列弹出，保证堆的大小一直为k
+#             if len(pri_que) > k:
+#                 heapq.heappop(pri_que)
+
+#         # 找出前k个高频元素，小顶堆先弹出的是小元素，因此倒序输出到数组中
+#         result = [0] * k
+#         for i in range(k-1, -1, -1):
+#             # 此处的[1]即取出其第一维，即元素而不是其频次
+#             result[i] = heapq.heappop(pri_que)[1]
+#         return result
+
 import heapq
+from collections import Counter
 class Solution:
     def topKFrequent(self, nums, k):
-        # 统计元素出现的频率
-        map_ = {}
-        for i in range(len(nums)):
-            map_[nums[i]] = map_.get(nums[i], 0) + 1
-        # 对频率排序，定义一个小顶堆，大小为k
-        pri_que = []
-        # 用固定大小为k的小顶堆，扫描所有频率的数值
-        for key, freq in map_.items():
-            heapq.heappush(pri_que, (freq, key))
-            # 如果堆的大小大于了k，则队列弹出，保证堆的大小一直为k
-            if len(pri_que) > k:
-                heapq.heappop(pri_que)
-
-        # 找出前k个高频元素，小顶堆先弹出的是小元素，因此倒序输出到数组中
-        result = [0] * k
-        for i in range(k-1, -1, -1):
-            # 此处的[1]即取出其第一维，即元素而不是其频次
-            result[i] = heapq.heappop(pri_que)[1]
-        return result
+        counter = Counter(nums)
+        hp = []
+        for num, freq in counter.items():
+            if len(hp) == k:
+                heapq.heappush(hp, (freq, num))
+                heapq.heappop(hp)
+            else:
+                heapq.heappush(hp, (freq, num))
+        return [t[1] for t in hp]
 
 
 if __name__ == "__main__":
