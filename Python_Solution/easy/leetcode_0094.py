462accf1c9c6a432bc5f469cc16887e56fa51ce8
Sirwenhao <1547102356@qq.com>
2022-07-18 16:39:32 +0800

    7/18

--- Python_Solution/easy/leetcode_0094.py
+++ Python_Solution/easy/leetcode_0094.py
@@ -9,14 +9,35 @@ class TreeNode:
         self.right = None
         self.left = None
 
+# class Solution:
+#     def inorderTraversal(self, root):
+#         ans = []
+#         def traversal(root):
+#             if root == None:
+#                 return
+#             traversal(root.left)
+#             ans.append(root.val)
+#             traversal(root.right)
+#         traversal(root)
+#         return ans
+
+# 2022/7/18  author:WH
+# 迭代法
 class Solution:
     def inorderTraversal(self, root):
+        if not root:
+            return []
         ans = []
-        def traversal(root):
-            if root == None:
-                return
-            traversal(root.left)
-            ans.append(root.val)
-            traversal(root.right)
-        traversal(root)
+        stack = []
+        cur = root
+        while cur or stack:
+            # 访问左子树的最底层节点
+            if cur:
+                stack.append(cur)
+                cur = cur.left
+            # 处理完最左子树之后，处理栈顶节点
+            else:
+                cur = stack.pop()
+                ans.append(cur.val)
+                cur = cur.right
         return ans
