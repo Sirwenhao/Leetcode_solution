6094702ab9cc87679e9fa5ba6ed50fd69370b92c
Sirwenhao <1547102356@qq.com>
2022-07-20 19:56:12 +0800

    7/20

--- Python_Solution/easy/leetcode_0429.py
+++ Python_Solution/easy/leetcode_0429.py
@@ -3,24 +3,42 @@
     1、二叉树问题，头疼
 """
 
-# leetcode官解，对树这种数据结构还是不熟悉
+# # leetcode官解，对树这种数据结构还是不熟悉
 
-from collections import deque
+# from collections import deque
 
-def levelOrder(root):
-    if not root:
-        return []
-    ans = list()
-    q = deque([root])
+# def levelOrder(root):
+#     if not root:
+#         return []
+#     ans = list()
+#     q = deque([root])
     
-    while q:
-        cnt = len(q)
-        level = list()
-        for _ in range(cnt):
-            cur = q.popleft()
-            level.append(cur.val)
-            for child in cur.children:
-                q.append(level)
-        ans.append(level)
-    return ans
->>>>>>> 01c7d28b36c2a0d6850f76944563b6ce436cd214
+#     while q:
+#         cnt = len(q)
+#         level = list()
+#         for _ in range(cnt):
+#             cur = q.popleft()
+#             level.append(cur.val)
+#             for child in cur.children:
+#                 q.append(level)
+#         ans.append(level)
+#     return ans
+
+# 2022/7/20  author:WH
+# N叉树的层序遍历
+class Solution:
+    def leverOrder(self, root):
+        if not root: return []
+        ans = []
+        # 把根节点添加进入到deque中，根节点的
+
+        que = deque([root])
+        while que:
+            result = []
+            for _ in range(len(que)):
+                cur = que.popleft()
+                result.append(cur.val)
+                if cur.children:
+                    que.extend(cur.children)
+            ans.append(result)
+        return ans
