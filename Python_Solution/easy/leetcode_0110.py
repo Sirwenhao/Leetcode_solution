4cf143708d2cbd18676240b8b4f6a6d6ce676364
Sirwenhao <1547102356@qq.com>
2022-07-24 21:14:00 +0800

    7/24

--- Python_Solution/easy/leetcode_0110.py
+++ Python_Solution/easy/leetcode_0110.py
@@ -24,6 +24,19 @@ class Solution:
                 height_map[real_node] = 1 + max(left, right)
         return True
 
+# 2022/7/23  author:WH
+class Solution:
+    def isBalanced(self, root):
+        if self.get_height(root) != -1: return True
+        else: return False
+
+    def get_height(self, root):
+        if not root: return 0
+        if (left_height == self.get_height(root.left)) == -1: return -1
+        if (right_height == self.get_height(root.right)) == -1: return -1
+        if abs(left_height - right_height) > 1: return -1
+        else: return 1 + max(left_height, right_height)
+
 
 
 # dict = {
