67c67dcc9d83b4a79d3eda9de056649c8d1003e7
Sirwenhao <1547102356@qq.com>
2022-07-30 20:32:50 +0800

    7/30

--- Python_Solution/middle/leetcode_0122.py
+++ Python_Solution/middle/leetcode_0122.py
@@ -6,14 +6,22 @@
 
 
 # 此算法应该有问题，奇怪的是竟然通过了leetcode的提交
+# class Solution:
+#     def maxProfits(self, prices):
+#         res = 0
+#         for i in range(len(prices)-1):
+#             if prices[i+1] > prices[i]:
+#                 res += prices[i+1]-prices[i]
+#         return res
+
+# 2022/7/30  author:代码随想录
+# 贪心算法，关键点在于正利润
 class Solution:
     def maxProfits(self, prices):
         res = 0
         for i in range(len(prices)-1):
-            if prices[i+1] > prices[i]:
-                res += prices[i+1]-prices[i]
+            res += max(prices[i+1] - prices[i], 0)
         return res
-
 
 # prices = [7,1,5,3,6,4,10,8,9,1,9]
 prices = [1,3,6,7,10]
