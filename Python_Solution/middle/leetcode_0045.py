645c0566e331f8a8ce1a00d682bf7204574e0f7c
Sirwenhao <1547102356@qq.com>
2022-07-31 14:46:56 +0800

    7/31

--- Python_Solution/middle/leetcode_0045.py
+++ Python_Solution/middle/leetcode_0045.py
@@ -3,33 +3,35 @@
     2、关键点在于最小步数
 """
 # 2022/7/31  author:WH
-# class Solution:
-#     def jump(self, nums):
-#         res = 0
-#         curC = 0
-#         nextC = 0
-#         for i in range(len(nums)):
-#             nextC = max(nextC, i+nums[i])
-#             if i == curC:
-#                 if curC != len(nums)-1:
-#                     res += 1
-#                     curC = nextC
-#                     if curC > len(nums)-1:
-#                         break
-#                 else:
-#                     break
-#         return res
-
-# 2022/7/31  author:github
 class Solution:
     def jump(self, nums):
-        end = coverage = steps = 0
-        for i,num in enumerate(nums[:-1]):
-            coverage = max(coverage, i+num)
-            if i == end:
-                end = coverage
-                steps += 1
-        return steps
+        if len(nums) == 1:
+            return 0
+        res = 0
+        curC = 0
+        nextC = 0
+        for i in range(len(nums)):
+            nextC = max(nextC, i+nums[i])
+            if i == curC:
+                if curC != len(nums)-1:
+                    res += 1
+                    curC = nextC
+                    if curC > len(nums)-1:
+                        break
+                else:
+                    break
+        return res
+
+# 2022/7/31  author:github
+# class Solution:
+#     def jump(self, nums):
+#         end = coverage = steps = 0
+#         for i,num in enumerate(nums[:-1]):
+#             coverage = max(coverage, i+num)
+#             if i == end:
+#                 end = coverage
+#                 steps += 1
+#         return steps
 
 
 
