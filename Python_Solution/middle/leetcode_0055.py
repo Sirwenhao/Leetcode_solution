bc88fee3910b7866e2694193e6b84fec5483e218
Sirwenhao <1547102356@qq.com>
2022-07-31 12:01:56 +0800

    7/31

--- Python_Solution/middle/leetcode_0055.py
+++ Python_Solution/middle/leetcode_0055.py
@@ -4,18 +4,32 @@
 """
 # 2022/7/29  author:WH
 
+# class Solution:
+#     def canJump(self, nums):
+#         Coverage = 0
+#         if len(nums) == 1: return True
+#         i = 0
+#         while i <= Coverage:
+#             Coverage = max(i + nums[i], Coverage)
+#             if Coverage >= len(nums)-1: return True
+#             i += 1
+#         return False
+
+# 2022/7/31  author:github
+
 class Solution:
     def canJump(self, nums):
         Coverage = 0
-        if len(nums) == 1: return True
-        i = 0
-        while i <= Coverage:
-            Coverage = max(i + nums[i], Coverage)
-            if Coverage >= len(nums)-1: return True
-            i += 1
-        return False
+        for i,num in enumerate(nums):
+            print('i:', i)
+            print('Coverage:', Coverage)
+            if i > Coverage:
+                return False
+            Coverage = max(Coverage, i + num)
+        return True
 
 if __name__ == "__main__":
-    nums = [3, 2, 1, 0, 4]
+    # nums = [3, 2, 1, 0, 4]
+    nums = [2,3,1,1,4]
     result = Solution().canJump(nums)
     print(result)
