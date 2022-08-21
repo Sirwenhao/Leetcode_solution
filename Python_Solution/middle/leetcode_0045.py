02706fa8a130771b7314348dfd0bc56187268f47
Sirwenhao <1547102356@qq.com>
2022-08-21 20:50:27 +0800

    8/21

--- Python_Solution/middle/leetcode_0045.py
+++ Python_Solution/middle/leetcode_0045.py
@@ -3,24 +3,24 @@
     2、关键点在于最小步数
 """
 # 2022/7/31  author:WH
-class Solution:
-    def jump(self, nums):
-        if len(nums) == 1:
-            return 0
-        res = 0
-        curC = 0
-        nextC = 0
-        for i in range(len(nums)):
-            nextC = max(nextC, i+nums[i])
-            if i == curC:
-                if curC != len(nums)-1:
-                    res += 1
-                    curC = nextC
-                    if curC > len(nums)-1:
-                        break
-                else:
-                    break
-        return res
+# class Solution:
+#     def jump(self, nums):
+#         if len(nums) == 1:
+#             return 0
+#         res = 0
+#         curC = 0
+#         nextC = 0
+#         for i in range(len(nums)):
+#             nextC = max(nextC, i+nums[i])
+#             if i == curC:
+#                 if curC != len(nums)-1:
+#                     res += 1
+#                     curC = nextC
+#                     if curC > len(nums)-1:
+#                         break
+#                 else:
+#                     break
+#         return res
 
 # 2022/7/31  author:github
 # class Solution:
@@ -33,6 +33,23 @@ class Solution:
 #                 steps += 1
 #         return steps
 
+# 2022/8/19  author:WH
+
+class Solution:
+    def jump(self, nums):
+        if len(nums) == 1:
+            return 0
+        ans = 0
+        curDistance = nextDistance = 0
+        for i in range(len(nums)):
+            nextDistance = max(i+nums[i], nextDistance)
+            if i == curDistance:
+                if curDistance != len(nums)-1:
+                    ans += 1
+                    curDistance = nextDistance
+                    if nextDistance >= len(nums)-1:
+                        break
+        return ans
 
 
 if __name__ == "__main__":
