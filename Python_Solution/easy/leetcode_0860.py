760babfbb580a4d84e74063d6e0ebafbda86bd6c
Sirwenhao <1547102356@qq.com>
2022-08-28 23:58:52 +0800

    8/28

--- Python_Solution/easy/leetcode_0860.py
+++ Python_Solution/easy/leetcode_0860.py
@@ -4,6 +4,30 @@
     3、贪心关键点在于遇到20美元，先用10美元解决
 """
 # 2022/8/1  author:WH
+# class Solution:
+#     def lemonadeChange(self, bills):
+#         five = ten = twenty = 0
+#         for i in bills:
+#             if i == 5:
+#                 five += 1
+#             elif i == 10:
+#                 if five <= 0: return False
+#                 ten += 1
+#                 five -= 1
+#             else:
+#                 if five > 0 and ten > 0:
+#                     five -= 1
+#                     ten -= 1
+#                     twenty += 1
+#                 elif five >= 3:
+#                     five -= 3
+#                     twenty += 1
+#                 else:
+#                     return False
+#         return True
+
+# 2022/8/28  author:WH
+# 关键点在于遇到面值为20的优先考虑使用面值10解决，如果不行，则使用两个5
 class Solution:
     def lemonadeChange(self, bills):
         five = ten = twenty = 0
@@ -11,7 +35,8 @@ class Solution:
             if i == 5:
                 five += 1
             elif i == 10:
-                if five <= 0: return False
+                if five < 1:
+                    return False
                 ten += 1
                 five -= 1
             else:
