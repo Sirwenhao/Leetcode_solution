40026b152f5f06eb7bee74c8fed3e5353c2297d0
Sirwenhao <1547102356@qq.com>
2022-08-29 21:57:30 +0800

    8/29

--- Python_Solution/middle/leetcode_0406.py
+++ Python_Solution/middle/leetcode_0406.py
@@ -4,18 +4,33 @@
     3、Python的lambda语句
 """
 # 2022/8/1  author:代码随想录
+# class Solution:
+#     def reconstructQueue(self, people):
+#         # 使用lambda语句进行排序
+#         # -x[0]是为了实现从大到校的排序，默认是从小到大
+#         # lambda语句可以实现首先按照-x[0]排序，当-x[0]相同时，按照x[1]从小到大排序
+#         people.sort(key=lambda x:(-x[0], x[1]))
+#         que = []
+#         # 根据每个元素的第二维度k，贪心算法，进行插入
+#         # 此时的people已经排序，同一身高条件下，k值小的排前面
+#         for p in people:
+#             que.insert(p[1], p)
+#         return que
+
+# 2022/8/29  author:WH（参考代码随想录）
 class Solution:
     def reconstructQueue(self, people):
-        # 使用lambda语句进行排序
-        # -x[0]是为了实现从大到校的排序，默认是从小到大
-        # lambda语句可以实现首先按照-x[0]排序，当-x[0]相同时，按照x[1]从小到大排序
-        people.sort(key=lambda x:(-x[0], x[1]))
-        que = []
-        # 根据每个元素的第二维度k，贪心算法，进行插入
-        # 此时的people已经排序，同一身高条件下，k值小的排前面
+        # 先排序，注意这条排序语句
+        people.sort(key=lambda x: (-x[0], x[1]))
+        # 排完序之后，按照人数进行插入即可
+        ans = []
         for p in people:
-            que.insert(p[1], p)
-        return que
+            # insert语句使用：list.insert(index, obj)
+            # index是要插入的索引位置
+            # obj是要插入的对象
+            ans.insert(p[1], p)
+        return ans
+
 
 if __name__ == "__main__":
     people = [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
