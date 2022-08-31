0456bb3fcc79387c594f8a01de10355e7fa33d20
Sirwenhao <1547102356@qq.com>
2022-08-31 22:10:24 +0800

    8/31

--- Python_Solution/middle/leetcode_0452.py
+++ Python_Solution/middle/leetcode_0452.py
@@ -22,31 +22,31 @@
 
 
 # 2022/8/29  author:WH
-# class Solution:
-#     def findMinArrowShots(self, points):
-#         # 先按照右边界排序
-#         points.sort(key = lambda x:x[0])
-#         ans = 1 # 至少需要1只箭
-#         for i in range(1, len(points)):
-#             if points[i][0] > points[i-1][1]:
-#                 ans += 1
-#             else:
-#                 # 如果重复的话，更新下最小右边界
-#                 points[i][1] = min(points[i-1][1], points[i][1])
-#         return ans
+class Solution:
+    def findMinArrowShots(self, points):
+        # 先按照右边界排序
+        points.sort(key = lambda x:x[0])
+        ans = 1 # 至少需要1只箭
+        for i in range(1, len(points)):
+            if points[i][0] > points[i-1][1]:
+                ans += 1
+            else:
+                # 如果重复的话，更新下最小右边界
+                points[i][1] = min(points[i-1][1], points[i][1])
+        return ans
 
 
 # 2022/8/29  author:github
-class Solution:
-    def findMinArrowShots(self, points):
-        points.sort(key=lambda x:x[1])
-        ans = 1
-        x = points[0][1]
-        for a,b in points:
-            if a > x:
-                ans += 1
-                x = b
-        return ans
+# class Solution:
+#     def findMinArrowShots(self, points):
+#         points.sort(key=lambda x:x[1])
+#         ans = 1
+#         x = points[0][1]
+#         for a,b in points:
+#             if a > x:
+#                 ans += 1
+#                 x = b
+#         return ans
 
 
 if __name__ == "__main__":
