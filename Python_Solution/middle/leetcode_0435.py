7e573de1d455d05a356ef77c48427af721e26feb
Sirwenhao <1547102356@qq.com>
2022-08-31 21:38:39 +0800

    8/31

--- Python_Solution/middle/leetcode_0435.py
+++ Python_Solution/middle/leetcode_0435.py
@@ -18,18 +18,35 @@
 #                 continue
 #         return ans
 
-# 2022/8/2  author:代码随想录
-# 逆向思维：需要统计的是需要移除的区间个数，那么可以反过来考虑总数-不需要移除的
+# # 2022/8/2  author:代码随想录
+# # 逆向思维：需要统计的是需要移除的区间个数，那么可以反过来考虑总数-不需要移除的
+# class Solution:
+#     def eraseOverlapIntervals(self, intervals):
+#         intervals.sort(key=lambda x:x[1])
+#         ans = 1
+#         end = intervals[0][1]
+#         for i in range(1, len(intervals)):
+#             if end <= intervals[i][0]:
+#                 end = intervals[i][1]
+#                 ans += 1
+#         return len(intervals)-ans
+
+# 2022/8/30  author:代码随想录
+# 本题与0452用最少数量的箭引爆气球非常像，弓箭的数量就相当于非交叉区间的个数
+# 差别在于如[a,b]和[b,c]之类的区间，被认为不是相邻区间
 class Solution:
     def eraseOverlapIntervals(self, intervals):
+        # 按照右边界进行排序
         intervals.sort(key=lambda x:x[1])
         ans = 1
-        end = intervals[0][1]
+        currentEnd = intervals[0][1]
         for i in range(1, len(intervals)):
-            if end <= intervals[i][0]:
-                end = intervals[i][1]
+            if intervals[i][0] >= currentEnd:
                 ans += 1
-        return len(intervals)-ans
+                # 更新终点，为下一次判断做准备
+                currentEnd = intervals[i][1]
+        return len(intervals) - ans
+
 
 if __name__ == "__main__":
     # intervals = [[1,2],[2,3],[3,4],[1,3]]
