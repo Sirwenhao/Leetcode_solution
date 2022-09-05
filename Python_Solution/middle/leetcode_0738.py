2027d8005f7b4879344455beaecee10838eab6fe
Sirwenhao <1547102356@qq.com>
2022-09-05 10:12:50 +0800

    9/5

--- Python_Solution/middle/leetcode_0738.py
+++ Python_Solution/middle/leetcode_0738.py
@@ -11,6 +11,23 @@ class Solution:
                 numStr[i:] = '9' * (len(numStr) - i)
         return int("".join(numStr))
 
+# 2022/9/4  author:github
+# class Solution:
+#     def monotoneIncreasingDigits(self, n):
+#         s = list(str(n))
+#         i = 1
+#         while i < len(s) and s[i-1] <= s[i]:
+#             i += 1
+#         if i < len(s):
+#             while i and s[i-1] > s[i]:
+#                 s[i-1] = str(int(s[i-1]) - 1)
+#                 i -= 1
+#             i += 1
+#             while i < len(s):
+#                 s[i] = '9'
+#                 i += 1
+#         return int("".join(s))
+
 if __name__ == "__main__":
     # n = 10
     n = 3324
