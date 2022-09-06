79241316e381329892599759e8e755baa8d5f0d4
Sirwenhao <1547102356@qq.com>
2022-09-06 19:41:40 +0800

    9/6

---
+++ Python_Solution/easy/leetcode_0746.py
@@ -0,0 +1,20 @@
+"""
+    1、746使用最小花费爬楼梯
+    2、重新考虑
+"""
+# 2022/9/6  author:WH
+class Solution:
+    def minCostClimbingStairs(self, cost):
+        dp = [0]*len(cost)
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+        for i in range(2, len(cost)):
+            dp[i] = min(dp[i-1], dp[i-2]) + cost[i]
+        # print(i)
+        return min(dp[len(cost)-2], dp[len(cost)-1])
+
+if __name__ == "__main__":
+    cost = [1,100,1,1,1,100,1,1,100,1]
+    # cost = [10, 15, 20]
+    result = Solution().minCostClimbingStairs(cost)
+    print(result)
