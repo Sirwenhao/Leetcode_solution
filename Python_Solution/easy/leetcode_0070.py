fa16fd0ed31db80ddbebbeaf548b49004e7613ab
Sirwenhao <1547102356@qq.com>
2022-09-06 11:56:23 +0800

    9/6

--- Python_Solution/easy/leetcode_0070.py
+++ Python_Solution/easy/leetcode_0070.py
@@ -4,16 +4,27 @@
 """
 # 2022/9/6  author:WH
 # 计算复杂度为O(n)
+# class Solution:
+#     def climbStairs(self, n):
+#         dp = [0] * (n+1)
+#         dp[0] = 1
+#         dp[1] = 1
+#         for i in range(2, n+1):
+#             dp[i] = dp[i-1] + dp[i-2]
+#         return dp[n]
+
+# 空间复杂度为O(1)的情况
 class Solution:
     def climbStairs(self, n):
-        if n <= 1:
-            return n
         dp = [0] * (n+1)
         dp[0] = 1
         dp[1] = 1
         for i in range(2, n+1):
-            dp[i] = dp[i-1] + dp[i-2]
-        return dp[i]
+            temp = dp[0] + dp[1]
+            dp[0] = dp[1]
+            dp[1] = temp
+        return dp[1]
+
 
 if __name__ == "__main__":
     n = 5
