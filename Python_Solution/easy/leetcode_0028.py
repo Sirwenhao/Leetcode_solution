d38067e8b2a26e41a31b3c5ef1fcaee197f7b59a
Sirwenhao <1547102356@qq.com>
2022-09-16 21:02:40 +0800

    9/16

--- Python_Solution/easy/leetcode_0028.py
+++ Python_Solution/easy/leetcode_0028.py
@@ -14,37 +14,49 @@
 #         return -1
 
 # 使用KMP算法
+# class Solution:
+#     def strStr(self, haystack, needle):
+#         a = len(haystack)
+#         b = len(needle)
+#         if a == 0:
+#             return 0
+#         next = self.getnext(a, needle)
+#         p = -1
+#         for j in range(b):
+#             while p >= 0 and needle[p+1] != haystack[j]:
+#                 p = next[p]
+#             if needle[p+1] == haystack[j]:
+#                 p+=1
+#             if p==a-1:
+#                 return j-a+1
+#         return -1
 
+#     def getnext(self, a, needle):
+#         next = ['' for i in range(a)]
+#         k=-1
+#         next[0] = k
+#         for i in range(1, len(needle)):
+#             while (k>-1 and needle[k+1]!=needle[i]):
+#                 k = next[k]
+#             if needle[k+1]==needle[i]:
+#                 k+=1
+#             next[i] = k
+#         return next
+
+# 2022/09/16  author:WH
+# 可以AC，但不是使用KMP解决的
 class Solution:
     def strStr(self, haystack, needle):
-        a = len(haystack)
-        b = len(needle)
-        if a == 0:
-            return 0
-        next = self.getnext(a, needle)
-        p = -1
-        for j in range(b):
-            while p >= 0 and needle[p+1] != haystack[j]:
-                p = next[p]
-            if needle[p+1] == haystack[j]:
-                p+=1
-            if p==a-1:
-                return j-a+1
+        for idx, val in enumerate(haystack):
+            if val == needle[:1] and idx + len(needle) <= len(haystack):
+                if haystack[idx:idx+len(needle)] == needle:
+                    return idx
+                else:
+                    continue
         return -1
 
-    def getnext(self, a, needle):
-        next = ['' for i in range(a)]
-        k=-1
-        next[0] = k
-        for i in range(1, len(needle)):
-            while (k>-1 and needle[k+1]!=needle[i]):
-                k = next[k]
-            if needle[k+1]==needle[i]:
-                k+=1
-            next[i] = k
-        return next
 if __name__ == "__main__":
-    haystack = "aaaaa"
-    needle = "bba"
+    haystack = "leetcode"
+    needle = "eet"
     result = Solution().strStr(haystack, needle)
     print(result)
