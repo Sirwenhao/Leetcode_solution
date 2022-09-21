6d8d9739bc942e232a30d4a9388e9e47ddb412ce
Sirwenhao <1547102356@qq.com>
2022-09-21 22:35:46 +0800

    9/21

--- Python_Solution/easy/leetcode_0203.py
+++ Python_Solution/easy/leetcode_0203.py
@@ -26,21 +26,39 @@
 
 # 2022/6/7 author:WH
 # 此处关于链表的习题有个直接经验是在头部添加虚拟节点dummy
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# class Solution:
+#     def removeElements(self, head, val):
+#         dummy_head = ListNode(next=head)
+#         cur = dummy_head
+#         while(cur.next != None):
+#             if cur.next.val == val:
+#                 cur.next = cur.next.next # 删除节点cur.next
+#             else:
+#                 cur = cur.next
+#         return dummy_head.next
+
+# 2022/09/21 author:WH
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
 class Solution:
     def removeElements(self, head, val):
-        dummy_head = ListNode(next=head)
-        cur = dummy_head
-        while(cur.next != None):
+        dummy = ListNode(0)
+        dummy.next = head
+        cur = dummy
+        while cur.next:
             if cur.next.val == val:
-                cur.next = cur.next.next # 删除节点cur.next
+                cur.next = cur.next.next
             else:
                 cur = cur.next
-        return dummy_head.next
-    
+        return dummy.next
+
 if __name__ == "__main__":
     head = [1,2,3,4,5,6,7]
     val = 6
