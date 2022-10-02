2497ecc4775a6d3a0f66a70469419edf90cf9eb9
Sirwenhao <1547102356@qq.com>
2022-10-02 10:39:38 +0800

    10/2

--- Python_Solution/easy/leetcode_0349.py
+++ Python_Solution/easy/leetcode_0349.py
@@ -2,31 +2,6 @@
     1、一开始理解的有点小问题，与顺序无关，就是单纯数学意义上的交集
 '''
 
-# def intersection(nums1, nums2):
-#     a = len(nums1)
-#     b = len(nums2)
-#     list = []
-#     if a > b:
-#         for i in range(b):
-#             if nums2[i] in nums1 and nums2[i] not in list:
-#                 list.append(nums2[i])
-#             else:
-#                 continue
-#     else:
-#         for i in range(a):
-#             if nums1[i] in nums2 and nums1[i] not in list:
-#                 list.append(nums1[i])
-#             else:
-#                 continue
-
-#     return list
-
-# nums1 = [1,2,2,1]
-# nums2 = [2,2]
-
-# result = intersection(nums1, nums2)
-# print(result)
-
 # 2022/6/12  author:WH
 # class Solution:
 #     def intersection(self, nums1, nums2):
@@ -36,15 +11,20 @@
 #         return list(nums1 & nums2)
 
 # 2022/6/19  author:WH
+# class Solution:
+#     def intersection(self, nums1, nums2):
+#         res = []
+#         nums1 = list(set(nums1))
+#         nums2 = list(set(nums2))
+#         for i in range(len(nums1)):
+#             if nums1[i] in nums2:
+#                 res.append([nums1[i]])
+#         return res
+
+# 2022/10/01  author:WH
 class Solution:
     def intersection(self, nums1, nums2):
-        res = []
-        nums1 = list(set(nums1))
-        nums2 = list(set(nums2))
-        for i in range(len(nums1)):
-            if nums1[i] in nums2:
-                res.append([nums1[i]])
-        return res
+        return list(set(nums1) & set(nums2))
 
 if __name__ == "__main__":
     nums1 = [1,2,3,4,5,6,2,1]
