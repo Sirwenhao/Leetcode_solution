89445a9e1a48dc318639d3c8cb4ea74c8628d6cf
Sirwenhao <1547102356@qq.com>
2022-11-13 15:14:38 +0800

    11/13

--- Python_Solution/middle/leetcode_0005.py
+++ Python_Solution/middle/leetcode_0005.py
@@ -121,25 +121,44 @@ class Solution:
 #         return s[start:start + maxLen]
 
 
-# 2022/11/12 author:WH
+# # 2022/11/12 author:WH
+# class Solution:
+#     def expand(self, s, left, right):
+#         while left >= 0 and right < len(s) and s[left] == s[right]:
+#             left -= 1
+#             right += 1
+#         return left+1, right-1
+
+
+#     def longestPalindrome(self, s):
+#         start, end = 0, 0
+#         for i in range(len(s)):
+#             left1, right1 = self.expand(s, i, i)
+#             left2, right2 = self.expand(s, i, i+1)
+#             if right1 - left1 > end - start:
+#                 start, end = left1, right1
+#             if right2 - left2 > end - start:
+#                 start, end = right2, left2
+#         return s[start:end+1]
+
+# 2022/11/13  author:WH
 class Solution:
     def expand(self, s, left, right):
         while left >= 0 and right < len(s) and s[left] == s[right]:
             left -= 1
             right += 1
         return left+1, right-1
-
-
+    
     def longestPalindrome(self, s):
         start, end = 0, 0
         for i in range(len(s)):
             left1, right1 = self.expand(s, i, i)
             left2, right2 = self.expand(s, i, i+1)
-            if right1 - left1 > end - start:
-                start, end = left1, right1
-            if right2 - left2 > end - start:
-                start, end = right2, left2
-        return s[start, end+1]
+            if right1-left1 > end-start:
+                end, start = right1, left1
+            if right2-left2 > end-start:
+                right2, left2 = end, start
+        return s[start:end+1]
 
 
 
