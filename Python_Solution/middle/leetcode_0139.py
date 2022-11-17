68309911763782bed857294644855b45f9b29762
Sirwenhao <1547102356@qq.com>
2022-11-18 00:06:10 +0800

    11/18

---
+++ Python_Solution/middle/leetcode_0139.py
@@ -0,0 +1,8 @@
+# 2022/11/17  author:WH
+# 主观想法，直接统计字符串及字典中所有字符出现的次数情况。
+# 如果字典中所有字符出现的次数少于字典中出现的，则可以拼出s
+from collections import Counter
+class Solution:
+    def wordBank(self, s, wordDict):
+        
+
