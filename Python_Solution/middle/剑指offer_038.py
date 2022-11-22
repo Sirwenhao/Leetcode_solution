d9be4068d1eed5d71321ddbf10d6eb4b5775c63f
Sirwenhao <1547102356@qq.com>
2022-11-22 20:06:04 +0800

    11/22

--- Python_Solution/middle/剑指offer_038.py
+++ Python_Solution/middle/剑指offer_038.py
@@ -85,33 +85,62 @@
 #         backtracking(s, 0, len(s))
 #         return res
 
-# 2022/11/20  author:WH
-# 没有办法解决重复字符问题，如aab
+# # 2022/11/20  author:WH
+# # 没有办法解决重复字符问题，如aab
+# class Solution:
+#     def __init__(self):
+#         self.ans = []
+#         self.current = ''
+
+#     def permutation(self, s):
+#         self.ans.clear()
+#         self.backtracking(s, 0)
+#         return self.ans
+        
+#     def backtracking(self, s, start_index):
+#         if len(self.current) == len(s):
+#             self.ans.append(self.current[:])
+#             return
+#         for i in range(len(s)):
+#             # 剪枝条件，如果当前字符已经在current中了，就跳过当前的这个找下一个
+#             if s[i] in self.current:
+#                 continue
+#             self.current += s[i]
+#             self.backtracking(s, start_index+1)
+#             self.current = self.current[:-1]
+
+# 2022/11/22 author:WH
+# 上述方法在去重的逻辑部分还不够完善，并不能解决一个字符只用一次的限定问题
+# 想法：开辟一组与字符串等长的标志位，默认全为False，如果用到就置为True
 class Solution:
     def __init__(self):
         self.ans = []
         self.current = ''
 
     def permutation(self, s):
+        flag = [False for _ in range(len(s))]
         self.ans.clear()
-        s = sorted(s)
-        self.backtracking(s, 0)
+        self.backtracking(s, 0, flag)
         return self.ans
-        
-    def backtracking(self, s, start_index):
+
+    def backtracking(self, s, start_index, flag):
         if len(self.current) == len(s):
-            self.ans.append(self.current[:])
+            self.ans.append(self.current)
             return
         for i in range(len(s)):
-            # 剪枝条件，如果当前字符已经在current中了，就跳过当前的这个找下一个
-            if i > 0 and s[i] == s[i-1]:
-                continue
+            # if flag[i]:
+            #     continue
+            flag[i] = True
             self.current += s[i]
-            self.backtracking(s, start_index+1)
+            self.backtracking(s, start_index+1, flag)
+            flag[i] = False
             self.current = self.current[:-1]
+
+
+
 
 if __name__ == "__main__":
     s = "ABC"
     result = Solution().permutation(s)
     print(result)
-        
+    
