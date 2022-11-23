d0d530b1491b80f048a9190cd6b144e8e45c8982
Sirwenhao <1547102356@qq.com>
2022-11-23 21:51:35 +0800

    11/23

--- CPP_Solution/middle/leetcode_0039.cpp
+++ CPP_Solution/middle/leetcode_0039.cpp
@@ -1,6 +1,7 @@
 // 2022/11/23  author:WH 组合总和
 #include<iostream>
 #include<vector>
+#include<algorithm> //sort函数的使用
 using namespace std;
 
 class Solution{
@@ -12,15 +13,14 @@ private:
             result.push_back(path);
             return;
         }
-    }
-    // 剪枝：如果sum+candidates[i] > target 就终止遍历
-    for (int i = startIndex; i < canidates.size() && sum + candidates[i] <= target; i++){
-        sum += candidates[i];
-        path.push_back(candidates[i]);
-        backtracking(candidates, target, sum, i);
-        sum -= candidates[i];
-        path.pop_back();
-    }
+        // 剪枝：如果sum+candidates[i] > target 就终止遍历
+        for (int i = startIndex; i < candidates.size() && sum + candidates[i] <= target; i++){
+            sum += candidates[i];
+            path.push_back(candidates[i]);
+            backtracking(candidates, target, sum, i);
+            sum -= candidates[i];
+            path.pop_back();
+        }
 }
 
 public:
