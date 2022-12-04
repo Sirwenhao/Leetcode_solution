06ae50a721222b19e47609919a97789f1a8440f0
Sirwenhao <1547102356@qq.com>
2022-12-04 19:47:09 +0800

    12/04

---
+++ CPP_Solution/middle/leetcode_0017.cpp
@@ -0,0 +1,20 @@
+// 2022/11/23 author:WH 电话号码的字母组合
+#include<iostream>
+#include<>
+using namespace std;
+
+class Solution{
+private:
+    const string letterMap[10] = {
+        "",
+        "",
+        "abc",
+        "def",
+        "ghi",
+        "jkl",
+        "mno",
+        "pqrs",
+        "tuv",
+        "wxyz",
+    };
+}
