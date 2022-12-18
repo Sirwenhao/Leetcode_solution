c5cc935b41098ac11b813884402d5df024e483aa
Sirwenhao <1547102356@qq.com>
2022-12-18 17:00:41 +0800

    12/18

--- Python_Solution/middle/leetcode_1764.py
+++ Python_Solution/middle/leetcode_1764.py
@@ -16,18 +16,32 @@
 # 贪心枚举：枚举nums中每一位nums[j]做为起点时是否与当前的groups[i]相匹配
 # 如果匹配则groups要跳到下一个元素，即i后移一位，同时指针j向后移动groups[i].length位
 # 如果不满足匹配，则将j后移一位
+# class Solution:
+#     def canChoose(self, groups, nums):
+#         n, m = len(groups), len(nums)
+#         i = j = 0
+#         while i < n and j < m:
+#             g = groups[i]
+#             if g == nums[j:j+len(g)]:
+#                 j += len(g)
+#                 i += 1
+#             else:
+#                 j += 1
+#         return i == n
+
+# 2022/12/18 review
 class Solution:
     def canChoose(self, groups, nums):
-        n, m = len(groups), len(nums)
+        m, n = len(groups), len(nums)
         i = j = 0
-        while i < n and j < m:
+        while i < m and j < n:
             g = groups[i]
             if g == nums[j:j+len(g)]:
                 j += len(g)
                 i += 1
             else:
                 j += 1
-        return i == n
+        return i == m
 
 
 if __name__ == "__main__":
