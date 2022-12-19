983fe6ffa89cd52cfcc4e5cbbd8102eea7d71d4c
Sirwenhao <1547102356@qq.com>
2022-12-19 22:50:27 +0800

    12/19

--- Python_Solution/hard/leetcode_0004.py
+++ Python_Solution/hard/leetcode_0004.py
@@ -17,10 +17,37 @@
 #             middle_num = '%.5f' %(nums[len(nums)//2])
 #         return middle_num
 
-# # 2022/11/15 author:github
+# # 2022/12/18 author:WH
+# 要求时间复杂度为O(log(m+n))，此种计算复杂度一般考虑二分法
+# class Solution:
+#     def findMedianSortedArrays(self, nums1, nums2):
+#         nums = nums1 + nums2
+#         index = len(nums) // 2
+
+#     def middleNum(self, array):
+#         if len(array) == 1:
+#             return '%.5f' %array[0]
+#         if len(array) == 2:
+#             return   '%.5f'%(array[0]+array[1])
+
+from cmath import inf
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2):
-
+        def findkth(i, j, k):
+            if i >= m:
+                return nums2[j+k-1]
+            if j >= n:
+                return nums1[i+k-1]
+            if k == 1:
+                return min(nums1[i], nums2[j])
+            midVal1 = nums1[i+k//2-1] if i+k//2-1 < m else inf
+            midVal2 = nums2[j+k//2-1] if j+k//2-1 < n else inf
+            if midVal1 < midVal2:
+                return findkth(i+k//2, j, k-k//2)
+            return findkth(i, j+k//2, k-k//2)
+        m, n = len(nums1), len(nums2)
+        left, right = (m+n+1) // 2, (m+n+2) // 2
+        return (findkth(0, 0, left) + findkth(0, 0, right)) / 2
 
 if __name__ == "__main__":
     nums1 = [1,3,5]
