276d47afec6760e029679ab3951f969333c8dcd8
Sirwenhao <1547102356@qq.com>
2022-12-20 22:30:32 +0800

    12/20

--- Python_Solution/easy/leetcode_1971.py
+++ Python_Solution/easy/leetcode_1971.py
@@ -1,5 +1,17 @@
 # 2022/12/19 author:github
 # 并查集
+# class Solution:
+#     def validPath(self, n, edges, source, destination):
+#         def find(x):
+#             if p[x] != x:
+#                 p[x] = find(p[x])
+#             return p[x]
+#         p = list(range(n))
+#         for u,v in edges:
+#             p[find(u)] = find(v)
+#         return find(source) == find(destination)
+
+# 2022/12/20  author:WH
 class Solution:
     def validPath(self, n, edges, source, destination):
         def find(x):
