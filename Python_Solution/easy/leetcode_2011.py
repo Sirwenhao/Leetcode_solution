0aec5c6399f86a7a526d373b665b7cc5def6c0c5
Sirwenhao <1547102356@qq.com>
2022-12-23 23:30:19 +0800

    12/23

---
+++ Python_Solution/easy/leetcode_2011.py
@@ -0,0 +1,23 @@
+# 2022/12/23  author:WH
+# 时间复杂度:O(N)
+# class Solution:
+#     def finalValueAfterOperations(self, operations):
+#         X = 0
+#         for i in operations:
+#             if i[:-1] == "--" or i[1:] == "--":
+#                 X -= 1
+#             elif i[:-1] == "++" or i[1:] == "++":
+#                 X += 1
+#         return X
+
+# 2022/12/23  author:官解
+# 跟第二次的想法不谋而合，只需要关注第二个元素即可
+class Solution:
+    def finalValueAfterOperations(self, operations):
+        return sum(1 if op[1] == "+" else -1 for op in operations)
+
+if __name__ == "__main__":
+    operations = ["--X","X++","X++"]
+    # operations = ["++X","++X","X++"]
+    result = Solution().finalValueAfterOperations(operations)
+    print(result)
