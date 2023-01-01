cfc2a77c64b6ab390607d29b81de916747f74e37
Sirwenhao <1547102356@qq.com>
2023-01-01 20:40:51 +0800

    1/1

--- Python_Solution/easy/leetcode_2351.py
+++ Python_Solution/easy/leetcode_2351.py
@@ -1,11 +1,21 @@
 # 2023/1/1  author:WH
+# class Solution:
+#     def repeatedCharacter(self, s):
+#         for i in range(1, len(s)):
+#             if s[i] not in s[:i]:
+#                 continue
+#             else:
+#                 return s[i]
+
+# 2023/1/1  author:github
+from collections import Counter
 class Solution:
     def repeatedCharacter(self, s):
-        for i in range(1, len(s)):
-            if s[i] not in s[:i]:
-                continue
-            else:
-                return s[i]
+        cnt = Counter()
+        for c in s:
+            cnt[c] += 1
+            if cnt[c] == 2:
+                return c
 
 if __name__ == "__main__":
     s = "abccbaacz"
