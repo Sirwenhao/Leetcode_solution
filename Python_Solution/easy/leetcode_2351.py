4c75e4a44f153a01cf5befa355b7fee412ab5406
Sirwenhao <1547102356@qq.com>
2023-01-01 20:47:29 +0800

    1/1

--- Python_Solution/easy/leetcode_2351.py
+++ Python_Solution/easy/leetcode_2351.py
@@ -8,14 +8,26 @@
 #                 return s[i]
 
 # 2023/1/1  author:github
-from collections import Counter
+# from collections import Counter
+# class Solution:
+#     def repeatedCharacter(self, s):
+#         cnt = Counter()
+#         for c in s:
+#             cnt[c] += 1
+#             if cnt[c] == 2:
+#                 return c
+
+# 2023/1/1  author:WH
+# mask中的第i位用于记录第i个字母是否出现过
 class Solution:
     def repeatedCharacter(self, s):
-        cnt = Counter()
+        mask = 0
         for c in s:
-            cnt[c] += 1
-            if cnt[c] == 2:
+            i = ord(c) - ord('a')
+            if mask >> i & 1: # >>左移
                 return c
+            mask |= 1 << i # |表示或 <<右移
+
 
 if __name__ == "__main__":
     s = "abccbaacz"
