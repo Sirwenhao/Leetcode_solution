8a66d6658f7e7dfe697eadaf0a7bb8b151aad6ec
Sirwenhao <1547102356@qq.com>
2023-01-06 20:50:08 +0800

    1/6

--- Python_Solution/easy/leetcode_2180.py
+++ Python_Solution/easy/leetcode_2180.py
@@ -1,15 +1,26 @@
 # 2023/1/6  author:WH
 # 枚举，时间复杂度:O(NlogN)，空间复杂度:O(1), 其中N=num
+# class Solution:
+#     def countEven(self, num):
+#         ans = 0
+#         for i in range(1, num+1):
+#             cur = 0
+#             while i != 0:
+#                 cur += i % 10
+#                 i //= 10
+#             ans += (cur % 2 == 0)
+#         return ans
+
 class Solution:
     def countEven(self, num):
-        ans = 0
-        for i in range(1, num+1):
-            cur = 0
-            while i != 0:
-                cur += i % 10
-                i //= 10
-            ans += (cur % 2 == 0)
+        ans = num // 10 * 5 - 1 # 每一个a~a+10的分数段中包含多少个偶数
+        x, s = num // 10, 0
+        while x:
+            s += x % 10
+            x //= 10
+        ans += (num % 10 + 2 - (s & 1)) >> 1
         return ans
+
     
 if __name__ == "__main__":
     result = Solution().countEven(30)
