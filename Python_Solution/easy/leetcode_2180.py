c248c5de0eb0a45e3151aac692025feb9eb36e30
Sirwenhao <1547102356@qq.com>
2023-01-06 20:52:08 +0800

    1/6

--- Python_Solution/easy/leetcode_2180.py
+++ Python_Solution/easy/leetcode_2180.py
@@ -11,6 +11,7 @@
 #             ans += (cur % 2 == 0)
 #         return ans
 
+# 时间复杂度 O(logn)，空间复杂度 O(1)。其中 n 为 num 的值。
 class Solution:
     def countEven(self, num):
         ans = num // 10 * 5 - 1 # 每一个a~a+10的分数段中包含多少个偶数
