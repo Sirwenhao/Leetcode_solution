6145046dbef9de873c397b640937cac4b306a059
Sirwenhao <1547102356@qq.com>
2023-01-08 22:12:12 +0800

    1/8

--- Python_Solution/easy/leetcode_21865.py
+++ Python_Solution/easy/leetcode_21865.py
@@ -15,10 +15,42 @@
 #         return ans
 
 # 2023/1/8 author:github
-# 一次遍历
+# 一次遍历, 时间复杂度O(n x m),空间复杂度O(1), 其中 n 和 m 分别是字符串数组 words 和字符串 pref 的长度。
+# class Solution:
+#     def prefixCount(self, words, pref):
+#         return sum(w.startswith(pref) for w in words)
+
+# 2023/1/8  author:github
+class Trie:
+    def __init__(self):
+        self.children = [None] * 26
+        self.cnt = 0
+
+    def insert(self, w):
+        node = self
+        for c in w:
+            i = ord(c) - ord('a')
+            if node.children[i] is None:
+                node.children[i] = Trie()
+            node = node.children[i]
+            node.cnt += 1
+
+    def search(self, pref):
+        node = self
+        for c in pref:
+            i = ord(c) - ord('a')
+            if node.children[i] is None:
+                return 0
+            node = node.children[i]
+        return node.cnt
+
+
 class Solution:
     def prefixCount(self, words, pref):
-        return sum(w.startswith(pref) for w in words)
+        tree = Trie()
+        for w in words:
+            tree.insert(w)
+        return tree.search(pref)
 
 
 if __name__ == "__main__":
