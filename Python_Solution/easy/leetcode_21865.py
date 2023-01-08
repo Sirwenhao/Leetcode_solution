f7730d734f3795b6cfe8c3dca69b2213f3646ff6
Sirwenhao <1547102356@qq.com>
2023-01-08 21:55:37 +0800

    1/8

---
+++ Python_Solution/easy/leetcode_21865.py
@@ -0,0 +1,22 @@
+# 2023/1/8  author:WH
+# 自己写的解法: 时间复杂度O(N),空间复杂度:O(1)
+class Solution:
+    def prefixCount(self, words, pref):
+        ans = 0
+        i = 0
+        n = len(pref)
+        while i < len(words):
+            if len(words[i]) >= n and words[i][:n] == pref:
+                ans += 1
+                i += 1
+            else:
+                i += 1
+                continue
+        return ans
+
+
+if __name__ == "__main__":
+    words = ["pay","attention","practice","attend"]
+    pref = "at"
+    result = Solution().prefixCount(words, pref)
+    print(result)
