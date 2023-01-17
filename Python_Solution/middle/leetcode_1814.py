f09f87a593259f8382c8412b2b9c8ecf68a20df5
Sirwenhao <1547102356@qq.com>
2023-01-17 20:44:33 +0800

    2023/1/17

--- Python_Solution/middle/leetcode_1814.py
+++ Python_Solution/middle/leetcode_1814.py
@@ -1,17 +1,28 @@
 # 2023/1/17  author:WH
 # 时间复杂度过高，O(n*(n-1)), 空间复杂度O(1)
 
+# class Solution:
+#     def countNicePairs(self, nums):
+#         ans = 0
+#         for i in range(len(nums)):
+#             for j in range(i+1, len(nums)):
+#                 if nums[i] + int(str(nums[j])[::-1]) == nums[j] + int(str(nums[i])[::-1]):
+#                     ans += 1
+#                 else:
+#                     continue
+#         return ans
+
+# leetcode官解
+from collections import Counter
 class Solution:
     def countNicePairs(self, nums):
         ans = 0
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + int(str(nums[j])[::-1]) == nums[j] + int(str(nums[i])[::-1]):
-                    ans += 1
-                else:
-                    continue
-        return ans
-
+        cnt = Counter()
+        for i in nums:
+            j = int(str(i)[::-1])
+            ans += cnt[i - j]
+            cnt[i - j] += 1
+        return ans % (10 ** 9 + 7)
 
 if __name__ ==  "__main__":
     nums = [42, 11, 1, 97]
