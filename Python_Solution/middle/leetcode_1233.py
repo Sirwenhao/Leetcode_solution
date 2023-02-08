b42cad09bfd6c7a8197f8f9dc70414f3f0911a56
Sirwenhao <1547102356@qq.com>
2023-02-09 00:25:33 +0800

    2023/2/9

--- Python_Solution/middle/leetcode_1233.py
+++ Python_Solution/middle/leetcode_1233.py
@@ -1,20 +1,30 @@
 # 2023/2/8  author:WH
 
-# author:WH 核心想法是排序之后进行元素对比
+# # author:WH 核心想法是排序之后进行元素对比
+# class Solution:
+#     def removeSubfolders(self, folder):
+#         folder.sort()
+#         # print(folder)
+#         ans = folder[:1]
+#         for i in folder[1:]:
+#             # print(ans[-1][:2])
+#             print(i[:len(ans[-1])])
+#             # 起初漏掉了此处if语句中的and部分
+#             if i[:len(ans[-1])] == ans[-1] and i[len(ans[-1]):len(ans[-1])+1] == '/':
+#                 continue
+#             else:
+#                 # print(i)
+#                 ans.append(i)
+#         return ans
+
+# leetcode官解
 class Solution:
     def removeSubfolders(self, folder):
         folder.sort()
-        # print(folder)
-        ans = folder[:1]
-        for i in folder[1:]:
-            # print(ans[-1][:2])
-            print(i[:len(ans[-1])])
-            # 起初漏掉了此处if语句中的and部分
-            if i[:len(ans[-1])] == ans[-1] and i[len(ans[-1]):len(ans[-1])+1] == '/':
-                continue
-            else:
-                # print(i)
-                ans.append(i)
+        ans = [folder[0]]
+        for i in range(1, len(folder)):
+            if not ((pre := len(ans[-1])) < len(folder[i]) and ans[-1] == folder[i][:pre] and folder[i][pre] == "/"):
+                ans.append(folder[i])
         return ans
 
 if __name__ == "__main__":
