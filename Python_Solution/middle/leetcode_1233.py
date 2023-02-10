103c42321a1e8aacacbd5c5d79ef9e8022d637e0
Sirwenhao <1547102356@qq.com>
2023-02-10 21:21:49 +0800

    2023/2/10

--- Python_Solution/middle/leetcode_1233.py
+++ Python_Solution/middle/leetcode_1233.py
@@ -39,8 +39,37 @@
 #         return ans
 
 # 字典树
+class Trie:
+    def __init__(self):
+        self.children = {}
+        self.fid = -1
+
+    def insert(self, i, f):
+        node = self
+        ps = f.split('/')
+        for p in ps[1:]:
+            if p not in node.children:
+                node.children[p] = Trie()
+            node = node.children[p]
+        node.fid = i
+
+    def search(self):
+        def dfs(root):
+            if root.fid != -1:
+                ans.append(root.fid)
+                return
+            for child in root.children.values():
+                dfs(child)
+        ans = []
+        dfs(self)
+        return ans
+
 class Solution:
     def removeSubfolders(self, folder):
+        trie = Trie()
+        for i, f in enumerate(folder):
+            trie.insert(i, f)
+        return [folder[i] for i in trie.search()]
 
 
 if __name__ == "__main__":
