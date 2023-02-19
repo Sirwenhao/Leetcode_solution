ccc4a34244075e24ec5174618e44d7eb3481b797
Sirwenhao <1547102356@qq.com>
2023-02-19 22:03:43 +0800

    2023/2/19

--- Python_Solution/middle/leetcode_1237.py
+++ Python_Solution/middle/leetcode_1237.py
@@ -13,3 +13,13 @@ class Solution:
                 ans.append([x, y])
         return ans
     
+# github解法
+from bisect import bisect_left
+class Solution:
+    def findSolution(self, customfunction, z):
+        ans = []
+        for x in range(1, z+1):
+            y = 1 + bisect_left(range(1, z+1), z, key=lambda y:customfunction.f(x, y))
+            if customfunction.f(x, y) == z:
+                ans.append([x, y])
+        return ans
