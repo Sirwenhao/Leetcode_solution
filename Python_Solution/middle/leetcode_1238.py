b55da7b3e73bf034c69e1bd17b66dd2a4308752f
Sirwenhao <1547102356@qq.com>
2023-03-23 20:51:09 +0800

    23/3/23

---
+++ Python_Solution/middle/leetcode_1238.py
@@ -0,0 +1,19 @@
+# 2023/2/23  author:WH
+
+# 列表全排列，判断两个整数的二进制有几位不同（相异或再计算其不同位数个数）
+
+# class Solution:
+#     def circularPermutation(self, n, start):
+#         # lst = list(i for i in range(2^n-1))  # 这种写法不对吗？？？
+#         l = []
+#         for i in range(2^n-1):
+#             l.append(i)
+#         print(l)
+
+
+
+# if __name__ == "__main__":
+#     n = 3
+#     start = 3
+#     result = Solution().circularPermutation(n, start)
+#     print(result)
