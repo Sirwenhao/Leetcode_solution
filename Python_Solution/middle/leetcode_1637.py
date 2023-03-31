cfe747f14e00e1923a91b27d0f4869e00a16f5a7
Sirwenhao <1547102356@qq.com>
2023-03-31 20:05:02 +0800

    23/3/31

--- Python_Solution/middle/leetcode_1637.py
+++ Python_Solution/middle/leetcode_1637.py
@@ -1,13 +1,19 @@
 # 2023/3/31  author:WH
 # 没有看懂题意，但是这个题真的不难。。。
 
+# class Solution:
+#     def maxWithOfVerticalArea(self, points):
+#         points.sort()
+#         ans = 0
+#         for i in range(len(points)-1):
+#             ans = max(ans, points[i+1][0]-points[i][0])
+#         return ans
+# github解法
+from itertools import pairwise
 class Solution:
     def maxWithOfVerticalArea(self, points):
         points.sort()
-        ans = 0
-        for i in range(len(points)-1):
-            ans = max(ans, points[i+1][0]-points[i][0])
-        return ans
+        return max(b[0] - a[0] for a,b in pairwise(points))
     
 if __name__ == "__main__":
     points = [[3,1],[9,0],[1,0],[1,4],[5,3],[8,8]]
