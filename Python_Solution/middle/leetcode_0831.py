c5eb0b66fd397b65f46f519f27e8f68c0560b145
Sirwenhao <1547102356@qq.com>
2023-04-01 21:25:58 +0800

    23/4/1

--- Python_Solution/middle/leetcode_0831.py
+++ Python_Solution/middle/leetcode_0831.py
@@ -10,6 +10,16 @@ class Solution:
         surf = '***-***-' + s[-4:]
         return surf if cnt == 0 else f'+{"*" * cnt}-{surf}'
     
+# # leetcode官解
+# class Solution:
+#     def maskPII(self, s):
+#         at = s.find('@')
+#         if at >= 0:
+#             return (s[0] + '*'*5 + s[at-1:]).lower()
+#         s = "".join(i for i in s if i.isdigit())
+#         return ["", "+*-", "+**-", "+***-"][len(s) - 10] + "***-***-" + s[-4:]
+
+    
 if __name__ == "__main__":
     # s = "LeetCode@LeetCode.com"
     s = '1(234)567-890'
