d87d035e519f7969ea5c3f25c70f50f03aa0d946
Sirwenhao <1547102356@qq.com>
2023-04-25 21:21:26 +0800

    2023/4/25

--- Python_Solution/easy/leetcode_2418.py
+++ Python_Solution/easy/leetcode_2418.py
@@ -1,9 +1,16 @@
 # 2023/4/25  author:WH
 
-# 创建二元数组，按照身高排序，返回排序后的人名.时间复杂度：O(nlongn)，空间复杂度O(n)
+# # 创建二元数组，按照身高排序，返回排序后的人名.时间复杂度：O(nlongn)，空间复杂度O(n)
+# class Solution:
+#     def sortPeople(self, names, heights):
+#         return [value for _,value in sorted(zip(heights, names), reverse=True)]
+
 class Solution:
     def sortPeople(self, names, heights):
-        return [value for _,value in sorted(zip(heights, names), reverse=True)]
+        idx = list(range(len(heights)))
+        # print(idx)
+        idx.sort(key=lambda i: -heights[i])
+        return [names[i] for i in idx]
     
 if __name__ == "__main__":
     names = ["Mary", "John", "Emma"]
