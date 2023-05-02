6df5a399b2664ba7ccee4d2afda493d632b72ca7
Sirwenhao <1547102356@qq.com>
2023-05-02 21:53:11 +0800

    2023/5/2

--- Python_Solution/middle/leetcode_0970.py
+++ Python_Solution/middle/leetcode_0970.py
@@ -1,24 +1,43 @@
 # 2023/5/2  author:WH
 
-# 枚举：但是提交答案超时了
+# # 枚举：但是提交答案超时了
+# class Solution:
+#     def powerfulIntegers(self, x, y, bound):
+#         ans = set()
+#         i = j = 0
+#         while x ** i <= bound:
+#             i += 1
+#         iMax = i
+#         while y ** j <= bound:
+#             j += 1
+#         jMax = j
+#         for m in range(iMax):
+#             for n in range(jMax):
+#                 res = x ** m + y ** n
+#                 if res <= bound:
+#                     ans.add(res)
+#                 else:
+#                     break
+#         return list(ans)
+
+# author:github
 class Solution:
     def powerfulIntegers(self, x, y, bound):
         ans = set()
-        i = j = 0
-        while x ** i <= bound:
-            i += 1
-        iMax = i
-        while y ** j <= bound:
-            j += 1
-        jMax = j
-        for m in range(iMax):
-            for n in range(jMax):
-                res = x ** m + y ** n
-                if res <= bound:
-                    ans.add(res)
-                else:
+        a = 1
+        while a <= bound:
+            b = 1
+            while a + b <= bound:
+                ans.add(a + b)
+                b *= y
+                if y == 1:
                     break
+            if x == 1:
+                break
+            a *= x
         return list(ans)
+
+
     
 if __name__ == "__main__":
     x = 2
