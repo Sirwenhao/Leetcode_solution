a90b5664db218cbd489bceee1d19e0e93468312e
Sirwenhao <1547102356@qq.com>
2023-05-02 21:31:03 +0800

    2023/5/2

--- Python_Solution/easy/leetcode_0009.py
+++ Python_Solution/easy/leetcode_0009.py
@@ -19,11 +19,22 @@
 #         y = int(str(x)[::-1])
 #         return x == y
 
-# 左右移运算符的使用
+# # 左右移运算符的使用
+# class Solution:
+#     def isPalindrome(self, x):
+#         s = str(x)
+#         return s[:len(s) >> 1] == s[:-(len(s) >> 1)-1: -1]
+
+# 数学方法
 class Solution:
     def isPalindrome(self, x):
-        s = str(x)
-        return s[:len(s) >> 1] == s[:-(len(s) >> 1)-1: -1]
+        if x < 0 or (x and x % 10 == 0):
+            return False
+        y = 0
+        while y < x:
+            y = y * 10 + x % 10
+            x //= 10
+        return x in (y, y//10)
 
 if __name__ == "__main__":
     x = 121
