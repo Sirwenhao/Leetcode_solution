5c059fe0863a4f6872196a228f742e7026ca9ed1
Sirwenhao <1547102356@qq.com>
2023-05-04 21:23:10 +0800

    2023/5/4

--- Python_Solution/middle/leetcode_0012.py
+++ Python_Solution/middle/leetcode_0012.py
@@ -1,15 +1,23 @@
 # 2023/5/4  author:WH
 
+# class Solution:
+#     def intToRoman(self, num):
+#         vs = (1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
+#         cs = ('M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I')
+#         ans = []
+#         for c,v in zip(cs, vs):
+#             while num >= v:
+#                 num -= v
+#                 ans.append(c)
+#         return "".join(ans)
+    
 class Solution:
     def intToRoman(self, num):
-        vs = (1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
-        cs = ('M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I')
-        ans = []
-        for c,v in zip(cs, vs):
-            while num >= v:
-                num -= v
-                ans.append(c)
-        return "".join(ans)
+        M = ["", "M", "MM", "MMM"]
+        C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+        X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+        I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+        return M[num//1000] + C[(num%1000)//100] + X[(num%100)//10] + I[num%10]
     
 if __name__ == "__main__":
     num = 1998
