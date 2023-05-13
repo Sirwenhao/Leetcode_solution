675d1f2f7c6c88795d4fad656e3b5dbc50211005
Sirwenhao <1547102356@qq.com>
2023-05-13 20:00:19 +0800

    23/5/13

--- Python_Solution/easy/leetcode_2441.py
+++ Python_Solution/easy/leetcode_2441.py
@@ -1,16 +1,22 @@
-# 2023/5/13  author:WH
+# # 2023/5/13  author:WH
+# class Solution:
+#     def findMaxK(self, nums):
+#         nums.sort()
+#         ans = -1
+#         n = len(nums)
+#         while n > 0:
+#             if ((-1)*nums[n-1]) in nums:
+#                 ans = nums[n-1]
+#                 break
+#             n -= 1
+#         return ans
+
+# author:github
 class Solution:
     def findMaxK(self, nums):
-        nums.sort()
-        ans = -1
-        n = len(nums)
-        while n > 0:
-            if ((-1)*nums[n-1]) in nums:
-                ans = nums[n-1]
-                break
-            n -= 1
-        return ans
-    
+        s = set(nums)
+        return max((x for x in s if -x in s), default=-1)
+
 if __name__ == "__main__":
     nums = [-1, 2, -3, 3]
     result = Solution().findMaxK(nums)
