21d23d8045a34b2afba7ad4da97f31316e61e551
Sirwenhao <1547102356@qq.com>
2023-05-15 21:14:38 +0800

    23/5/15

--- Python_Solution/middle/leetcode_1054.py
+++ Python_Solution/middle/leetcode_1054.py
@@ -10,17 +10,41 @@
 #                 ans.append(barcodes[i])
 #         return ans
 
-from collections import Counter
+# from collections import Counter
+# class Solution:
+#     def rearrangeBarcodes(self, barcodes):
+#         cnt = Counter(barcodes)
+#         barcodes.sort(key=lambda x: (-cnt[x], x)) # 按照次数从大到小排序，如果次数一样就按照元素排序
+#         n = len(barcodes)
+#         ans = [0] * len(barcodes)
+#         ans[::2] = barcodes[:(n+1) // 2]
+#         ans[1::2] = barcodes[(n+1) // 2:]
+#         return ans
+
+# 利用堆
+import heapq
 class Solution:
     def rearrangeBarcodes(self, barcodes):
-        cnt = Counter(barcodes)
-        barcodes.sort(key=lambda x: (-cnt[x], x)) # 按照次数从大到小排序，如果次数一样就按照元素排序
+        count = {}
+        for barcode in barcodes:
+            count[barcode] = count.get(barcode, 0) + 1
+        # 构建最大堆, 将出现次数和条形码对插入堆中
+        max_heap = []
+        for barcode, freq in count.items():
+            heapq.heappush(max_heap, (-freq, barcode))
         n = len(barcodes)
-        ans = [0] * len(barcodes)
-        ans[::2] = barcodes[:(n+1) // 2]
-        ans[1::2] = barcodes[(n+1) // 2:]
+        ans = [0] * n
+        index = 0
+
+        # 填充奇数索引位置
+        while max_heap:
+            freq, barcode = heapq.heappop(max_heap)
+            for _ in range(-freq):
+                ans[index] = barcode
+                index += 2
+                if index >= n:
+                    index = 1
         return ans
-        
     
 if __name__ == "__main__":
     barcodes = [1,1,1,2,2,2]
