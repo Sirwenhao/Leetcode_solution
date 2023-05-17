d44ab77e55af66dae0f489d45d3877cb577b7a3e
Sirwenhao <1547102356@qq.com>
2023-05-17 22:10:06 +0800

    23/5/17

--- Python_Solution/easy/leetcode_2403.py
+++ Python_Solution/easy/leetcode_2403.py
@@ -1,9 +1,13 @@
 # 2023/5/17  author:WH
 
+# class Solution:
+#     def haveConflict(self, event1, event2):
+#         return (event2[1] >= event1[1] >= event2[0] or event1[1] >= event2[1] >= event1[0])
+
 class Solution:
     def haveConflict(self, event1, event2):
-        return (event2[1] >= event1[1] >= event2[0] or event1[1] >= event2[1] >= event1[0])
-    
+        return not(event1[1] < event2[0] or event2[1] < event1[0])
+
 if __name__ == "__main__":
     event1 = ["01:15", "02:00"]
     event2 = ["02:00", "03:00"]
