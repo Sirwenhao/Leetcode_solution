d2024ef6ae1e21d0e8f433488f48a569088632bc
Sirwenhao <1547102356@qq.com>
2023-06-02 22:06:57 +0800

    23/6/2

---
+++ Python_Solution/middle/leetcode_2559.py
@@ -0,0 +1,24 @@
+# 2023/6/2  author:WH
+
+# 超时，通过率92/93
+class Solution:
+    def vowelStrings(self, words, queires):
+        flag = ['a', 'e', 'i','o', 'u']
+        ans = []
+        for q in queires:
+            num = 0
+            # print(words[q[0]:q[1]+1])
+            for s in (words[q[0]:q[1]+1]):
+                if s[0] in flag and s[-1] in flag:
+                    num += 1
+            ans.append(num)
+            num = 0
+        return ans
+
+
+
+if __name__ == "__main__":
+    words = ["aba", "bcb", "ece", "aa", "e"]
+    queries = [[0,2], [1,4],[1,1]]
+    result = Solution().vowelStrings(words, queries)
+    print(result)
