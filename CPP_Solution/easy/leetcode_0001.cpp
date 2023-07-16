b379b27e331b5af92eaf7040be2472ceb698d0af
Sirwenhao <1547102356@qq.com>
2023-07-16 22:50:33 +0800

    23/7/16

--- CPP_Solution/easy/leetcode_0001.cpp
+++ CPP_Solution/easy/leetcode_0001.cpp
@@ -5,19 +5,19 @@
 
 using namespace std;
 
-// class Solution{
-// public:
-// vector<int> twoSum(vector<int>& nums, int target){
-//     unordered_map<int, int> m;
-//     for(int i=0; i<nums.size();i++)
-//     {
-//         int v = nums[i];
-//         int x = target - v;
-//         if(m.count(x)) return {m[x], i};
-//         m[v]=i;
-//     }
-//     return {};
-// };
+class Solution{
+public:
+vector<int> twoSum(vector<int>& nums, int target){
+    unordered_map<int, int> m;
+    for(int i=0; i<nums.size();i++)
+    {
+        int v = nums[i];
+        int x = target - v;
+        if(m.count(x)) return {m[x], i};
+        m[v]=i;
+    }
+    return {};
+};
 
 
 int main(){
