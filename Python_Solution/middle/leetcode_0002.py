89cc24b6e9d9920ef567c3534dcfbe82e89a9b88
Sirwenhao <1547102356@qq.com>
2023-10-19 22:37:30 +0800

    23/110/19

--- Python_Solution/middle/leetcode_0002.py
+++ Python_Solution/middle/leetcode_0002.py
@@ -146,3 +146,24 @@ class Solution:
             l2 = l2.next if l2 else None
         return dummy.next
 
+# 23/10/18 author:WH
+# 还是沿用之前的逻辑
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        ans = 0
+        l3 = ListNode(0) # l3指向一个空结点
+        head = l3 # 用一个头结点指向l3，用于维护下属计算
+        carry = 0
+        while l1 or l2 or carry:
+            sum = ((l1.val if l1 else 0) + (l2.val if l2 else 0) + carry) # 此处l1和l2为0时的判断忘记了
+            carry, ans = divmod(sum, 10) # divmod用法忘记了
+            head.next = ListNode(ans)  # ListNode(ans)把ans转换成结点值
+            l1 = l1.next if l1 else None # 这两处的判断逻辑忘记
+            l2 = l2.next if l2 else None 
+            head = head.next # 结点后移，继续维护运算
+        return l3.next
