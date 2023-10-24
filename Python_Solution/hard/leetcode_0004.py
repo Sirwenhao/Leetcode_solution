5660f60551e3f7cee5b3025d32cc0347a05f3dcb
Sirwenhao <1547102356@qq.com>
2023-10-24 23:01:09 +0800

    23/10/24

--- Python_Solution/hard/leetcode_0004.py
+++ Python_Solution/hard/leetcode_0004.py
@@ -30,24 +30,37 @@
 #         if len(array) == 2:
 #             return   '%.5f'%(array[0]+array[1])
 
-from cmath import inf
+# from cmath import inf
+# class Solution:
+#     def findMedianSortedArrays(self, nums1, nums2):
+#         def findkth(i, j, k):
+#             if i >= m:
+#                 return nums2[j+k-1]
+#             if j >= n:
+#                 return nums1[i+k-1]
+#             if k == 1:
+#                 return min(nums1[i], nums2[j])
+#             midVal1 = nums1[i+k//2-1] if i+k//2-1 < m else inf
+#             midVal2 = nums2[j+k//2-1] if j+k//2-1 < n else inf
+#             if midVal1 < midVal2:
+#                 return findkth(i+k//2, j, k-k//2)
+#             return findkth(i, j+k//2, k-k//2)
+#         m, n = len(nums1), len(nums2)
+#         left, right = (m+n+1) // 2, (m+n+2) // 2
+#         return (findkth(0, 0, left) + findkth(0, 0, right)) / 2
+
+
+# 23/10/24 author：WH
+# 有点惊讶，这也能过。。。
+
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2):
-        def findkth(i, j, k):
-            if i >= m:
-                return nums2[j+k-1]
-            if j >= n:
-                return nums1[i+k-1]
-            if k == 1:
-                return min(nums1[i], nums2[j])
-            midVal1 = nums1[i+k//2-1] if i+k//2-1 < m else inf
-            midVal2 = nums2[j+k//2-1] if j+k//2-1 < n else inf
-            if midVal1 < midVal2:
-                return findkth(i+k//2, j, k-k//2)
-            return findkth(i, j+k//2, k-k//2)
-        m, n = len(nums1), len(nums2)
-        left, right = (m+n+1) // 2, (m+n+2) // 2
-        return (findkth(0, 0, left) + findkth(0, 0, right)) / 2
+        nums3 = sorted(nums1 + nums2)
+        length = len(nums3)
+        if length % 2 == 0:
+            return (nums3[length//2] + nums3[length//2+1])/2
+        else:
+            return nums3[length//2]
 
 if __name__ == "__main__":
     nums1 = [1,3,5]
