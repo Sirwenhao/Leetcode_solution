31251dea482f3de46cb89396830801a01a540dbb
Sirwenhao <1547102356@qq.com>
2023-10-25 23:04:22 +0800

    23/10/25

--- Python_Solution/middle/leetcode_0005.py
+++ Python_Solution/middle/leetcode_0005.py
@@ -141,35 +141,60 @@ class Solution:
 #                 start, end = right2, left2
 #         return s[start:end+1]
 
-# 2022/11/13  author:WH
-class Solution:
-    def expand(self, s, left, right):
-        while left >= 0 and right < len(s) and s[left] == s[right]:
-            left -= 1
-            right += 1
-        return left+1, right-1
+# # 2022/11/13  author:WH
+# class Solution:
+#     def expand(self, s, left, right):
+#         while left >= 0 and right < len(s) and s[left] == s[right]:
+#             left -= 1
+#             right += 1
+#         return left+1, right-1
     
+#     def longestPalindrome(self, s):
+#         start, end = 0, 0
+#         for i in range(len(s)):
+#             left1, right1 = self.expand(s, i, i)
+#             left2, right2 = self.expand(s, i, i+1)
+#             if right1-left1 > end-start:
+#                 end, start = right1, left1
+#             if right2-left2 > end-start:
+#                 right2, left2 = end, start
+#         return s[start:end+1]
+
+# 23/10/25 author:WH
+
+class Solution:
     def longestPalindrome(self, s):
-        start, end = 0, 0
-        for i in range(len(s)):
-            left1, right1 = self.expand(s, i, i)
-            left2, right2 = self.expand(s, i, i+1)
-            if right1-left1 > end-start:
-                end, start = right1, left1
-            if right2-left2 > end-start:
-                right2, left2 = end, start
-        return s[start:end+1]
+        ans = 0
+        res = ''
+        # 定义回文字符串的判断逻辑
+        def palindRome(s):
+            if s == s[::-1]:
+                return True
+            return False
+        l = len(s)
+        if l == 1:
+            return s
+        slow = 0
+        while slow < l:
+            for fast in range(slow+1, l+1):
+                if palindRome(s[slow:fast]) and len(s[slow:fast]) > ans:
+                    res = s[slow:fast]
+                    ans = len(s[slow:fast])
+                else:
+                    continue
+            slow += 1
+        return res
+
+        
 
 
-
-
-if __name__ == "__main__":
-    s = 'abcbabb'
-    result = Solution().longestPalindrome(s)
-    print(result)
+# if __name__ == "__main__":
+#     s = 'abcbabb'
+#     result = Solution().longestPalindrome(s)
+#     print(result)
 
 if __name__ == '__main__':
-    s = "babad"
+    s = "ac"
     result = Solution().longestPalindrome(s)
     print(result)
 
