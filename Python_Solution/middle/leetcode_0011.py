1b766d5caa65a9bd44487b522e4e293dcfafc855
Sirwenhao <1547102356@qq.com>
2023-11-02 23:41:14 +0800

    23/11/02 如何在这个基础上进一步优化，降低时间复杂度？

--- Python_Solution/middle/leetcode_0011.py
+++ Python_Solution/middle/leetcode_0011.py
@@ -70,20 +70,34 @@
 #                 right -= 1
 #         return ans
 
-# 2022/11/13 author:WH
+# # 2022/11/13 author:WH
+# class Solution:
+#     def maxArea(self, height):
+#         left, right = 0, len(height)-1
+#         ans = 0
+#         while left < right:
+#             area = (right-left)*min(height[left], height[right])
+#             ans = max(ans, area)
+#             if height[left] < height[right]:
+#                 left += 1
+#             else:
+#                 right -= 1
+#         return ans
+
+# 23/11/02 author:WH
+# 核心点：找到使min(left, right)*(index(right)-index(left))值最大的情况，并返回此时的最大值
 class Solution:
     def maxArea(self, height):
         left, right = 0, len(height)-1
         ans = 0
         while left < right:
-            area = (right-left)*min(height[left], height[right])
+            area = min(height[left], height[right]) * (right-left)
             ans = max(ans, area)
             if height[left] < height[right]:
                 left += 1
             else:
                 right -= 1
         return ans
-
 
 if __name__ == "__main__":
     heights = [1,8,6,2,5,4,8,3,7]
