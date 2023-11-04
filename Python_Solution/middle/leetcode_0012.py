32ee90a37f37743c26c903b54d87d2c4b96bc7a2
Sirwenhao <1547102356@qq.com>
2023-11-05 00:12:19 +0800

    23/11/04

--- Python_Solution/middle/leetcode_0012.py
+++ Python_Solution/middle/leetcode_0012.py
@@ -11,15 +11,26 @@
 #                 ans.append(c)
 #         return "".join(ans)
     
+# class Solution:
+#     def intToRoman(self, num):
+#         M = ["", "M", "MM", "MMM"]
+#         C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+#         X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+#         I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+#         return M[num//1000] + C[(num%1000)//100] + X[(num%100)//10] + I[num%10]
+    
+
+# 23/11/04 author:WH
+# 以下解法，无意中看到了之前的
 class Solution:
-    def intToRoman(self, num):
-        M = ["", "M", "MM", "MMM"]
-        C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
-        X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
-        I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
-        return M[num//1000] + C[(num%1000)//100] + X[(num%100)//10] + I[num%10]
+    def intToRoman(self, num: int) -> str:
+        a = ["","M","MM","MMM"]
+        b = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"]
+        c = ["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"]
+        d = ["","I","II","III","IV","V","VI","VII","VIII","IX"]
+        return ''.join(a[num//1000]+b[(num%1000)//100]+c[(num%100)//10]+d[(num%10)])
     
 if __name__ == "__main__":
-    num = 1998
+    num = 2998
     result = Solution().intToRoman(num)
     print(result)
