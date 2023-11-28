5bed3b7db4a86e0b30e624954c56a78382ca45a1
wenhao <1547102356@qq.com>
2023-11-28 23:31:06 +0800

    23/11/28

--- Python_Solution/easy/leetcode_0027.py
+++ Python_Solution/easy/leetcode_0027.py
@@ -20,14 +20,14 @@
 # 2022/7/2  author:WH
 # 双指针：快慢指针
 # 版本1：存在的问题是使用for循环改变不了数组中出现连续val值的情况
-# class Solution:
-#     def removeElement(self, nums, val):
-#         slow = 0
-#         for fast in range(0, len(nums)-1):
-#             if nums[fast] == val:
-#                 slow = fast
-#                 nums[fast], nums[fast+1] = nums[fast+1], nums[fast]
-#         return slow
+class Solution:
+    def removeElement(self, nums, val):
+        slow = 0
+        for fast in range(0, len(nums)-1):
+            if nums[fast] == val:
+                slow = fast
+                nums[fast], nums[fast+1] = nums[fast+1], nums[fast]
+        return slow
 
 # 版本2
 # 主要是利用nums[fast] != val进行判断
@@ -53,17 +53,31 @@
 #             # fast += 1
 #         return slow
 
-# author:WH
+# # author:WH
+# class Solution:
+#     def removeElement(self, nums, val):
+#         slow = fast = 0
+#         while fast < len(nums):
+#             # 一步一步把每一个不等于val的值送到前面去
+#             if nums[fast] != val:
+#                 nums[slow], nums[fast] = nums[fast], nums[slow]
+#                 slow += 1
+#             fast += 1
+#         return slow
+
+
+# # 23/11/28 author:WH
 class Solution:
     def removeElement(self, nums, val):
         slow = fast = 0
-        while fast < len(nums):
-            # 一步一步把每一个不等于val的值送到前面去
+        for fast in range(len(nums)):
             if nums[fast] != val:
-                nums[slow], nums[fast] = nums[fast], nums[slow]
+                nums[slow] = nums[fast]
                 slow += 1
-            fast += 1
-        return slow
+            else:
+                continue
+        return fast
+
 
 if __name__ == "__main__":
     nums = [0,1,2,2,3,0,4,2]
